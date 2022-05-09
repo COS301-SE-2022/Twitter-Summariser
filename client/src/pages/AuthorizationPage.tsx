a59f4cb8c74ef1a45babb86db8bc5ada170b282f
jonahgasura <u20592061@tuks.co.za>
2022-05-09 22:54:22 +0200

    ✨ Add main component files for authorization pages

---
+++ client/src/pages/AuthorizationPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import LoginBody from "../components/authorization-components/LoginBody";
+import LoginHeader from "../components/authorization-components/LoginHeader"
+// import SignUpBody from "../components/authorization-components/SignUpBody";
+// import SignUpHeader from "../components/authorization-components/SignUpHeader"
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+        <LoginHeader/>
+        <LoginBody/>
+    </div>
+  )
+}
+
+// const SignUpPage = () => {
+//   return (
+//     <div className="signup-container">
+//         <SignUpHeader/>
+//         <SignUpBody/>
+//     </div>
+//   )
+// }
+
+export default LoginPage;
+
+// export default SignUpPage;
