f9c142cc25af80d413aeaa9dd61271e874c06f8b
jonahgasura <u20592061@tuks.co.za>
2022-05-10 13:15:13 +0200

    ✨ Add main component files for landing page

---
+++ client/src/components/common-components/SearchBar.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="search-component">
+      <input placeholder="Search Twitter"/>
+    </div>
+  )
+}
+
+export default SearchBar;
