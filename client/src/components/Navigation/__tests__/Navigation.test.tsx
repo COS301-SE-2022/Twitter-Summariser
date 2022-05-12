1f42d78d902ef7e597720b61d12ca26adedb0ffa
malcolmdzimati <malcolmdzimati@gmail.com>
2022-05-12 03:02:12 +0200

    :white_check_mark::twisted_rightwards_arrows:Added Rendering Tests for Landing and Navigation

--- client/src/components/Navigation/__tests__/Navigation.test.tsx
+++ client/src/components/Navigation/__tests__/Navigation.test.tsx
@@ -3,3 +3,10 @@ import { render, screen } from "@testing-library/react"
 import ReactDOM from "react-dom"
 
 import Navigation from "../Navigation"
+
+//Test to see if Navigation is rendered
+it("renders without crashing", () => {
+    const { getByTestId } = render(<Navigation />);
+    const navi = getByTestId("navi");
+    expect(navi).toBeTruthy();
+});
