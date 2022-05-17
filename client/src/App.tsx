d15ecb8b571f2815cdda96ba8fc3fbefd03248a4
GO-Shoderu <go.shoderu@gmail.com>
2022-05-17 22:27:14 +0200

    🚸 🚧 🚀 ⚡️ 🎨 ✨ (Added the Login Component Functionality)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,88 @@
+import { useState } from "react";
 import Landing from "./components/Landing/Landing";
-// import Login from "./components/Login/Login";
-// import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 import "./index.css";
+
+// importing mock data
+import tweeter from "./mock.json";
 
 // main Application component in which different page sub-components will be contained
 const App = () => {
-  // state function comes here
+  const [loginPage, changeLoginPage] = useState(true);
+  const [correctLoginDetails, changeCorrectLoginDetails] = useState(false);
+  const [landingPage, changeLandingPage] = useState(false);
+  const [signupPage, changeSignupPage] = useState(false);
+  const [user_id, changeUser_id] = useState(0.0);
+
+  if (correctLoginDetails === signupPage) {
+    //do nothing, just bypassing warnings
+  }
+
+  // retrieving user login details
+  const userAuthentication = (props: any) => {
+    console.log(props);
+
+    const data = tweeter.users;
+
+    for (let index = 0; index < data.length; index++) {
+      if (
+        (data[index].name.toLowerCase() === props.username.toLowerCase() &&
+          data[index].password
+            .toLowerCase()
+            .match(props.password.toLowerCase())) ||
+        data[index].email.toLowerCase() ===
+          (props.username.toLowerCase() &&
+            data[index].password
+              .toLowerCase()
+              .match(props.password.toLowerCase()))
+      ) {
+        console.log(data[index]);
+
+        changeLoginPage(false);
+        changeCorrectLoginDetails(true);
+        changeLandingPage(true);
+        changeSignupPage(false);
+
+        changeUser_id(data[index].id);
+        return;
+      }
+    }
+  };
+
+  const signUpPage = (props: any) => {
+    changeLoginPage(false);
+    changeCorrectLoginDetails(false);
+    changeLandingPage(false);
+    changeSignupPage(true);
+
+    changeUser_id(0.0);
+  };
+
+  const logInPage = (props: any) => {
+    changeLoginPage(false);
+    changeCorrectLoginDetails(false);
+    changeLandingPage(true);
+    changeSignupPage(false);
+
+    changeUser_id(0.0);
+  };
 
   return (
     <div className="">
       {/* Login */}
-      {/* <Login /> */}
+      {loginPage && (
+        <Login
+          userLoginDetails={userAuthentication}
+          takeToSignupPage={signUpPage}
+        />
+      )}
+
       {/* Signup */}
-      {/* <Signup /> */}
+      {signupPage && <Signup />}
+
       {/* Entry here based on Signup and Login decision  */}
-      <Landing />;
+      {landingPage && <Landing userID={user_id} takeToLoginPage={logInPage} />}
     </div>
   );
 };
