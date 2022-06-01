c9aa0898730f6b3677b32ecd158013ad74cdbe83
Tlholo Koma <tlholokoma@gmail.com>
2022-06-01 07:47:54 +0200

    :test_tube: :construction: :white_check_mark: writing scripts for integration test of Creator table

--- src/services/__tests__/creator.service.int.ts
+++ src/services/__tests__/creator.service.int.ts
@@ -4,7 +4,6 @@ import {CreateTableInput, DeleteTableInput} from "aws-sdk/clients/dynamodb";
 import CreatorServices from "..";
 import {DynamoDB} from "aws-sdk";
 import dynamoDBClient from "@model/database";
-import { getAllCreators } from "@functions/creator";
 import Creator from "@model/creator/creator.model";
 
 const dynamoDb : DynamoDB = new DynamoDB({ ... dynamoDBClient()});
@@ -68,21 +67,73 @@ describe("creator.service", () => {
         await dynamoDb.deleteTable(params).promise();
     });
 
-    describe("getAllCreators", () => {
+    describe("getCreator", () => {
         describe("on error", () => {
-            it ("error should be thrown if no item is found in database", () => {
-                return expect(CreatorServices.creatorService.getAllCreators()).rejects.toThrowError(
-                    "no records found"
+            it("should throw an error if no item is found in the database", () => {
+                const email = "test@yahoo.com";
+                const password = "password";
+
+                return expect(CreatorServices.creatorService.getCreator(email, password)).rejects.toThrowError(
+                    "creator not found"
                 );
             });
         });
 
-        // describe("on success", () => [
-        //     it ("should return correct creator", async () => {
-        //         const creator: Creator = {
-                    
-        //         }
-        //     })
-        // ])
-    })
-})
+        describe("on success", () => {
+            it("should return the correct creator", async () => {
+                const creator : Creator= {
+                    apiKey: "AbcDwu9dgfv",
+                    username: "test",
+                    email: "test@yahoo.com",
+                    password: "password",
+                    dateOfBirth: "2002/01/08",
+                    dateRegistered: "2021-01-08T00:00:00.000Z"
+                };
+
+                await CreatorServices.creatorService.addCreator(creator);
+            
+                return expect(CreatorServices.creatorService.getCreator(creator.email, creator.password)).resolves.toMatchInlineSnapshot(`
+                    Object {
+                        "apiKey": "AbcDwu9dgfv",
+                        "email": "test@yahoo.com",
+                        "username": "test"
+                    }
+                `);
+            });
+        });
+    });
+
+    describe("addCreator", () => {
+        describe("on error", () => {
+            it("should throw an error if no creator is provided", () => {
+                const creator = null;
+
+                return expect(CreatorServices.creatorService.addCreator(creator)).rejects.toThrowError(
+                    "no new creator provided"
+                );
+            });
+        });
+
+        describe("on success", () => {
+            it("should create and return the correct creator", async () => {
+                const creator : Creator= {
+                    apiKey: "AbzDwu9dYfv",
+                    username: "testAdd",
+                    email: "testAdd@yahoo.com",
+                    password: "passwordAdd",
+                    dateOfBirth: "2002/02/08",
+                    dateRegistered: "2021-01-08T00:00:00.000Z"
+                };
+
+                return expect(CreatorServices.creatorService.addCreator(creator)).resolves.toMatchInlineSnapshot(`
+                    Object {
+                        "apiKey": "AbzDwu9dYfv",
+                        "email": "testAdd@yahoo.com",
+                        "username": "testAdd"
+                    }
+                `);
+            });
+        });
+    });
+    
+});
