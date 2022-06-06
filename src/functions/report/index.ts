c79c1ab8bc53bf159fad845434990a5ba64b196b
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-07 01:11:47 +0200

    :memo::art::bulb:Added in code that describes report lambda functions

--- src/functions/report/index.ts
+++ src/functions/report/index.ts
@@ -2,6 +2,7 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export const getAllReports = {
     handler: `${handlerPath(__dirname)}/handler.getAllReports`,
+    describe: 'A function that returns all of the reports in the system.',
     events: [
         {
             http: {
@@ -15,6 +16,7 @@ export const getAllReports = {
 
 export const getAllMyReports = {
     handler: `${handlerPath(__dirname)}/handler.getAllMyReports`,
+    describe: 'A function that returns all the users reports.',
     events: [
         {
             http: {
@@ -28,6 +30,7 @@ export const getAllMyReports = {
 
 export const getReport = {
     handler: `${handlerPath(__dirname)}/handler.getReport`,
+    describe: 'A function that returns the content of a specific report.',
     events: [
         {
             http: {
