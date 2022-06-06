d5a94e97fcb673d102efcdf8fa463bf5bd0b0c4d
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-07 01:07:12 +0200

    :memo::art::fire::bulb:Added in code that describes result set lambda function

--- src/functions/resultSet/index.ts
+++ src/functions/resultSet/index.ts
@@ -2,6 +2,7 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export const getAllResultSet = {
     handler: `${handlerPath(__dirname)}/handler.getAllResultSet`,
+    describe: 'Function that returns all the resultsets a specific user has.',
     events: [
         {
             http: {
@@ -15,7 +16,7 @@ export const getAllResultSet = {
 
 export const getResultSet = {
     handler: `${handlerPath(__dirname)}/handler.getResultSet`,
-    describe: 'Function that returns the contents of a result set',
+    describe: 'Function that returns the contents of a result set, e.g tweets and resultset info.',
     events: [
         {
             http: {
