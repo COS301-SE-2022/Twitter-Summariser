1b1878d4f6e337edce3b5da37c5ccf30937114c0
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-07 01:12:56 +0200

    :memo::art::bulb:Added in code that describes generate report lambda functions

--- src/functions/generateReport/index.ts
+++ src/functions/generateReport/index.ts
@@ -2,6 +2,7 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export const generateReport = {
     handler: `${handlerPath(__dirname)}/handler.generateReport`,
+    describe: 'A function that generates a report.',
     events: [
         {
             http: {
