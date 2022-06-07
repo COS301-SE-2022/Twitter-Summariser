34dc21208919dfcb98a00527c61f048e3d9290b0
jonahgasura <u20592061@tuks.co.za>
2022-06-07 11:01:09 +0200

    ✨ Added Splash page component features

---
+++ client/src/components/Splash/Splash.tsx
@@ -0,0 +1,49 @@
+import "./Splash.css";
+import Logo from "../Logo/Logo";
+
+import { useState } from "react";
+
+const Signup = (props: any) => {
+
+  return (
+    <div
+      data-testid="signup"
+      className="flex justify-center flex-col items-center h-screen"
+    >
+      <div>
+        <Logo width="136px" height="121px" page="login" />
+      </div>
+
+      <div>
+        <h1 className="text-center text-xl font-bold">Create your account</h1>
+      </div>
+      <br />
+      {/* unacceptable name notification */}
+      {/*  */}
+      <div>
+          <div className="flex flex-row">
+            &nbsp;
+          </div>
+          <button
+            data-testid="btn-signup"
+            className="button__login text-sm p-0.5 h-10 w-56 bg-black rounded-full text-white"
+          >
+            Sign up
+          </button>
+          <br />
+          <p className="text-sm text-center">
+            Already have an account?
+            <button
+              data-testid="btn-login"
+              className="button__login text-sm p-0.5 h-10 w-56 bg-black rounded-full text-white"
+            >
+              Login
+            </button>
+              &nbsp; Sign in
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
