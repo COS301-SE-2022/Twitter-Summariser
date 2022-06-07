f60516d6ddf7387e77d1762db4c0d3725336312b
Tlholo Koma <tlholokoma@gmail.com>
2022-06-07 13:46:24 +0200

    :fire: file issues

--- src/model/Report/report.model.ts
+++
@@ -1,7 +0,0 @@
-export default interface Report {
-    reportID: string,
-    resultSetID: string,
-    apiKey: string,
-    dateCreated: Date,
-    author: string
-};
--- src/model/report/report.model.ts
+++
@@ -1,8 +0,0 @@
-export default interface Report {
-    reportID: string,
-    resultSetID: string,
-    apiKey: string,
-    dateCreated: Date,
-    author: string,
-    tweets?: any
-};
