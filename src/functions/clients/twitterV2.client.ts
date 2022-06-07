1fc5661a5a6cf0e98cc5031dd3f7fd0fb7fb37fd
Zenthon <u20536951@tuks.co.za>
2022-06-07 10:39:15 +0200

    ✨ 🐛 (CICD/API): Temporal fix for Twitter API

--- src/functions/clients/twitterV2.client.ts
+++ src/functions/clients/twitterV2.client.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 export const clientV2 = new Twitter({
     //consumer_key: process.env.TWITTER_CONSUMER_KEY,
     //consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.BEARER_TOKEN
+    bearer_token: "AAAAAAAAAAAAAAAAAAAAAFPkcQEAAAAAQimZJHCD5ZqRX6B9sRmp5V0amjQ%3DxkoUm8DdJo7ClfpIoZy2Wcj4AvDN5tF7EvZUaYPT2A2gvSgtY1"
     //access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     //access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
