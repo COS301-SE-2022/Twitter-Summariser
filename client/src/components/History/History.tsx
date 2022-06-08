847a248a113c1244597318795234126e6fdc885b
GO-Shoderu <go.shoderu@gmail.com>
2022-06-08 20:04:52 +0200

    🚧 (UI): Added the Endpoint

--- client/src/components/History/History.tsx
+++ client/src/components/History/History.tsx
@@ -1,7 +1,172 @@
-import React from "react";
+// import { useState } from "react";
+
+// importing mock data
+// import tweeter from "../../mock.json";
 
 const History = () => {
-  return <div>History</div>;
+  // const [enteredSearch, changeEnteredSearch] = useState("");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // const searchHandler = (event: any) => {
+  //   changeEnteredSearch(event.target.value);
+  // };
+
+  // ######################### API FOR SEARCHING ###############################################
+
+  const getAllResultSetEndpoint =
+    "https://czbmusycz2.execute-api.us-east-1.amazonaws.com/dev/getAllResultSet";
+
+  const getHistory = async () => {
+    // POST request using fetch with error handling
+
+    const apiData = {
+      apiKey: localStorage.getItem("loggedUserApi"),
+    };
+
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(apiData),
+    };
+
+    fetch(getAllResultSetEndpoint, requestOptions)
+      .then(async (response) => {
+        const isJson = response.headers
+          .get("content-type")
+          ?.includes("application/json");
+
+        const data = isJson && (await response.json());
+
+        console.log(await data);
+
+        // console.log(await data.tweets);
+        // changeResultSet(await data.resultSetID);
+        // changeResponse(await data.tweets);
+
+        // check for error response
+        if (!response.ok) {
+          // error
+          // signUpFailure(true);
+
+          return;
+        }
+
+        // await props.readyToLogIN();
+      })
+      .catch((error) => {
+        console.log("Error Getting History");
+        // signUpFailure(true);
+      });
+  };
+
+  getHistory();
+
+  // #######################################################################
+
+  return (
+    <div>
+      {/* search */}
+      {/* <div className="flex justify-center p-2 border-l border-r border-b border-gray-200">
+        <div className="w-3/4 mb-3">
+          <input
+            data-testid="search"
+            type="search"
+            className="
+                nosubmit
+                w-full
+                px-3
+                py-1.5
+                text-lg
+                font-normal
+                text-gray-700
+                bg-clip-padding
+                border border-solid border-gray-300
+                rounded-lg
+                focus:text-gray-700 focus:bg-white focus:border-twitter-blue focus:outline-none
+                bg-gray-200
+              "
+            value={enteredSearch}
+            onChange={searchHandler}
+            placeholder="search your published report..."
+          />
+        </div>
+      </div> */}
+
+      {/* Api response comes here */}
+      <div className=" mt-16">
+        {/* {enteredSearch !== "" && (
+          <h1 className="text-2xl">Search results....</h1>
+        )} */}
+
+        {/* <div className="mt-4 flex flex-row flex-wrap justify-center">
+          {tweeter.myReports.map(
+            (data) =>
+              data.title.toLowerCase().match(enteredSearch.toLowerCase()) &&
+              enteredSearch !== "" && (
+                <div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
+                  <div className="">
+                    <button type="submit">
+                      <p className="font-bold">{data.title}</p>
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <p className="italic text-xs">{data.name}</p>
+                  </div>
+                  <div className="">
+                    <p className="italic text-xs">{data.date}</p>
+                  </div>
+                </div>
+              )
+          )}
+        </div> */}
+
+        {/* {enteredSearch === "" && (
+          <div className=" mt-4">
+            <h1 className="text-2xl">My Reports</h1>
+
+            <div className="mt-4 flex flex-row flex-wrap justify-center">
+              {tweeter.myReports.map((data) => (
+                <div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
+                  <div className="">
+                    <button type="submit">
+                      <p className="font-bold">{data.title}</p>
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <p className="italic text-xs">{data.name}</p>
+                  </div>
+                  <div className="">
+                    <p className="italic text-xs">{data.date}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )} */}
+
+        <div className=" mt-4">
+          <h1 className="text-3xl">Search Result History</h1>
+
+          <div className="mt-4 flex flex-row flex-wrap justify-center">
+            {/* {tweeter.myReports.map((data) => (
+                <div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
+                  <div className="">
+                    <button type="submit">
+                      <p className="font-bold">{data.title}</p>
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <p className="italic text-xs">{data.name}</p>
+                  </div>
+                  <div className="">
+                    <p className="italic text-xs">{data.date}</p>
+                  </div>
+                </div>
+              ))} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default History;
