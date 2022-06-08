577f14ae8890c55ce9430eebe880455dac0da75e
Tlholo Koma <tlholokoma@gmail.com>
2022-06-08 19:44:50 +0200

    :sparkles: :card_file_box: :wrench: (DB) add styles table for text in report

---
+++ src/model/textStyles/textStyles.model.ts
@@ -0,0 +1,9 @@
+export default interface TextStyle {
+    styleID: string,
+    reportBlockID: string,
+    align: string,
+    bold: string,
+    colour: string,
+    italic: string,
+    size: string
+}
