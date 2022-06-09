f538d15baeacaa05ce4094e8285c84086f4bee80
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-09 02:40:58 +0200

    :zap::sparkles:Add in update block

--- src/functions/editBlock/handler.ts
+++ src/functions/editBlock/handler.ts
@@ -2,17 +2,20 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import ServicesLayer from "../../services";
+import { randomUUID } from "crypto";
 
 export const editBlock = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         const params = JSON.parse(event.body);
 
-        const block = await ServicesLayer.reportBlock.getReportBlock(params.reportID);
-
-        if(block == undefined){
-          //await ServicesLayer.reportBlock.addReportBlock({});
+        if(params.reportBlockID==undefined){
+          var id = "BL-"+randomUUID();
+          await ServicesLayer.reportBlockService.addReportBlock({reportBlockID: id, reportID: params.reportID, blockType: 'RICHTEXT', position: params.position, richText: params.text});
+          var sid = "ST-"+randomUUID();
+          await ServicesLayer.textStyleService.addStyle({styleID: sid, reportBlockID: id, align: params.textStyle.Align, bold: params.textStyle.Bold, colour: params.textStyle.Color, italic: params.textStyle.Italic, size: params.textStyle.Size});
         }else{
-          //await ServicesLayer.reportBlock.updateReportBlock({});
+          await ServicesLayer.reportBlockService.addReportBlock({reportBlockID: params.reportBlockID, reportID: params.reportID, blockType: 'RICHTEXT', position: params.position, richText: params.text});
+          await ServicesLayer.textStyleService.addStyle({styleID: params.styleID, reportBlockID: params.reportBlockID, align: params.textStyle.Align, bold: params.textStyle.Bold, colour: params.textStyle.Color, italic: params.textStyle.Italic, size: params.textStyle.Size});
         }
         
         return {
