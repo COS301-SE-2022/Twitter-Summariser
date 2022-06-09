32f3f978cf887683904d2508e48382e6a4edb7a0
GO-Shoderu <go.shoderu@gmail.com>
2022-06-09 09:40:35 +0200

    ✨ (UI): Retrieving data from API and Created new components

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
   const [enteredSearch, changeEnteredSearch] = useState("");
   const [resultSet, changeResultSet] = useState("");
   const [date, changeDate] = useState("");
+  const [genReport, changeGenReport] = useState("");
 
   const searchHandler = (event: any) => {
     changeEnteredSearch(event.target.value);
@@ -40,15 +41,14 @@ const Home = () => {
 
         const data = isJson && (await response.json());
 
-        changeDate(await data.Report.dateCreated.substring(0, 10));
-
-        localStorage.setItem("id", await data.Report.reportID);
-
         if (!response.ok) {
           // error
 
           return;
         }
+
+        changeDate(await data.Report.dateCreated.substring(0, 10));
+        changeGenReport(data.Report.reportID);
 
         if (enteredSearch !== "") {
           changeCreateTitle(enteredSearch);
@@ -155,6 +155,12 @@ const Home = () => {
   );
 
   let ind = 0;
+
+  const viewGenReport = () => {
+    if (enteredSearch !== "") {
+      localStorage.setItem("draftReportId", genReport);
+    }
+  };
 
   return (
     <div data-testid="home">
@@ -269,10 +275,14 @@ const Home = () => {
         {clicked && (
           <div className="mt-4 flex flex-col flex-wrap justify-center">
             <h1 className="text-2xl">Newly created report</h1>
-            <Link to="genReport/">
+            <Link to="/genReport">
               <div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
                 <div className="">
-                  <button data-testid="btn-report" type="submit">
+                  <button
+                    data-testid="btn-report"
+                    type="submit"
+                    onClick={viewGenReport}
+                  >
                     <p className="font-bold">{createTitle}</p>
                   </button>
                 </div>
