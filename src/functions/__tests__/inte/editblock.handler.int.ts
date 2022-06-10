bacc7aee5182665c40d6a73cb0d9a203d086e13c
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-10 04:46:05 +0200

    :test_tube:Add code to test report endpoint

--- src/functions/__tests__/inte/editblock.handler.int.ts
+++ src/functions/__tests__/inte/editblock.handler.int.ts
@@ -1,6 +1,6 @@
 import * as request from 'superagent';
 
-describe('Given an authorised request WHEN the GET editBlock endpoint is called', () => {
+describe('Given an authorised request WHEN the post editBlock endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
         const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/editBlock");
         expect(statusCode).toBe(200);
