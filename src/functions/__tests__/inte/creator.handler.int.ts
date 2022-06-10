374ce7e10562f855e5efd65af85a72b7a2e34300
malcolmdzimati <malcolmdzimati@gmail.com>
2022-06-10 05:09:05 +0200

    :bug:Fixing failing tests

--- src/functions/__tests__/inte/creator.handler.int.ts
+++ src/functions/__tests__/inte/creator.handler.int.ts
@@ -29,13 +29,13 @@ describe('Given an authorised request WHEN the post signin endpoint is called', 
 
 describe('Given an authorised request WHEN the post signup endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
-        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/signup");
+        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/signup/");
         expect(statusCode).toBe(200);
         
     });
 
     test('THEN it should respond with a healthy body', async () => {
-        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/signup");
+        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/signup/");
         expect(body).toBeTruthy;
     });
 })
--- src/functions/__tests__/inte/report.handler.int.ts
+++ src/functions/__tests__/inte/report.handler.int.ts
@@ -2,39 +2,39 @@ import * as request from 'superagent';
 
 describe('Given an authorised request WHEN the post getAllReports endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
-        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllReports");
+        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllReports/");
         expect(statusCode).toBe(200);
         
     });
 
     test('THEN it should respond with a healthy body', async () => {
-        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllReports");
+        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllReports/");
         expect(body).toBeTruthy;
     });
 })
 
 describe('Given an authorised request WHEN the post getAllMyReport endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
-        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllMyReports");
+        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllMyReports/");
         expect(statusCode).toBe(200);
         
     });
 
     test('THEN it should respond with a healthy body', async () => {
-        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllMyReports");
+        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getAllMyReports/");
         expect(body).toBeTruthy;
     });
 })
 
 describe('Given an authorised request WHEN the post getReport endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
-        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getReport");
+        const { statusCode } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getReport/");
         expect(statusCode).toBe(200);
         
     });
 
     test('THEN it should respond with a healthy body', async () => {
-        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getReport");
+        const { body } = await request.post("https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/getReport/");
         expect(body).toBeTruthy;
     });
 })
