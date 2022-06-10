d3425e192d22d67ae5a903892f087d3cbf63178d
Tlholo Koma <tlholokoma@gmail.com>
2022-06-10 03:24:50 +0200

    :fire: (TEST) remove test config files

--- jest.int.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: "node",
-    roots: ["<rootDir>/src"],
-    testMatch: ["**/*.int.ts"],
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testTimeout: 60000
-};
