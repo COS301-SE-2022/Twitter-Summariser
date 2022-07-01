d1f826d301fc4f8c9ae2f251e3c124898a139633
GO-Shoderu <go.shoderu@gmail.com>
2022-07-01 12:21:26 +0200

    ⚡️ (UI): added delete icon

--- client/src/components/HistoryCard/HistoryCard.tsx
+++ client/src/components/HistoryCard/HistoryCard.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { MdDeleteOutline } from "react-icons/md";
 
 const HistoryCard = (props: any) => {
   // console.log(props.data);
+
+  const icon_style_3 = { fontSize: "1.5rem", color: "red" };
 
   const setResultSet = () => {
     console.log(props.data.id);
@@ -10,21 +13,31 @@ const HistoryCard = (props: any) => {
 
   return (
     <div>
-      <div className="">
-        <Link to="/viewHistory">
-          <button type="submit" onClick={setResultSet}>
-            <p className="font-bold">{props.data.searchPhrase}</p>
-          </button>
-        </Link>
-      </div>
-      <div className="mt-2">
-        <p className="italic text-xs">Sorted: {props.data.sortOption}</p>
-      </div>
-      <div className="mt-2">
-        <p className="italic text-xs">Filtered: {props.data.filterOption}</p>
-      </div>
-      <div className="">
-        <p className="italic text-xs">{props.data.date}</p>
+      <div className="flex flex-row justify-between items-center">
+        <div className="">
+          <div className="">
+            <Link to="/viewHistory">
+              <button type="submit" onClick={setResultSet}>
+                <p className="font-bold">{props.data.searchPhrase}</p>
+              </button>
+            </Link>
+          </div>
+          <div className="mt-2">
+            <p className="italic text-xs">Sorted: {props.data.sortOption}</p>
+          </div>
+          <div className="mt-2">
+            <p className="italic text-xs">
+              Filtered: {props.data.filterOption}
+            </p>
+          </div>
+          <div className="">
+            <p className="italic text-xs">{props.data.date}</p>
+          </div>
+        </div>
+
+        <div className=" ml-2" data-bs-toggle="tooltip" title="Delete History">
+          <MdDeleteOutline style={icon_style_3} />
+        </div>
       </div>
     </div>
   );
