e1621b365bd6422a838b3f7df52eb674cbb5df60
Tlholo Koma <tlholokoma@gmail.com>
2022-07-03 23:59:12 +0200

    :sparkles: :card_file_box: Add function to get permissions

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,6 +5,7 @@ import ReportBlockService from "./reportBlock.service";
 import ResultSetService from "./resultSet.service";
 import TweetService from "./tweet.service";
 import TextStyleService from "./textStyles.service";
+import PermissionsService from "./permissions.service";
 
 
 const reportService = new ReportService(dynamoDBClient());
@@ -13,6 +14,7 @@ const resultSetServices = new ResultSetService(dynamoDBClient());
 const tweetService = new TweetService(dynamoDBClient());
 const reportBlockService = new ReportBlockService(dynamoDBClient());
 const textStyleService = new TextStyleService(dynamoDBClient());
+const permissionsService = new PermissionsService(dynamoDBClient());
 
-export default { creatorService, resultSetServices, tweetService, reportService, reportBlockService, textStyleService };
+export default { creatorService, resultSetServices, tweetService, reportService, reportBlockService, textStyleService, permissionsService };
 
