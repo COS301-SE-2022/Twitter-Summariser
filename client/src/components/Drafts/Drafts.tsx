f77773e24544e485a66b3eb1b19295235535c6cb
Zenthon <u20536951@tuks.co.za>
2022-07-06 14:16:10 +0200

    ✨ (CICD/UI): Updated the way we get endpoints

--- client/src/components/Drafts/Drafts.tsx
+++ client/src/components/Drafts/Drafts.tsx
@@ -5,91 +5,91 @@ import DraftCard from "../DraftCard/DraftCard";
 import link from "../../resources/links.json";
 
 function Drafts() {
-	const [draft, changeDraft] = useState<any[]>([]);
+    const [draft, changeDraft] = useState<any[]>([]);
 
-	// localStorage.removeItem("resultSetId");
-	// localStorage.removeItem("draftReportId");
+    // localStorage.removeItem("resultSetId");
+    // localStorage.removeItem("draftReportId");
 
-	// ######################### API FOR GETTING HISTORY #####################
+    // ######################### API FOR GETTING HISTORY #####################
 
-	let getAllMyDraftReportsEndpoint = String(link.endpointLink);
-	getAllMyDraftReportsEndpoint += "getAllMyReports";
+    let getAllMyDraftReportsEndpoint = process.env.NODE_ENV === 'development' ? String(link.localhostLink) : String(link.serverLink);
+    getAllMyDraftReportsEndpoint += "getAllMyReports";
 
-	// using localhost
-	// const getAllMyDraftReportsEndpoint = "http://localhost:4000/dev/getAllMyReports";
+    // using localhost
+    // const getAllMyDraftReportsEndpoint = "http://localhost:4000/dev/getAllMyReports";
 
-	const getHistory = async () => {
-		const apiData = {
-			apiKey: localStorage.getItem("loggedUserApi")
-		};
+    const getHistory = async () => {
+        const apiData = {
+            apiKey: localStorage.getItem("loggedUserApi")
+        };
 
-		const requestOptions = {
-			method: "POST",
-			body: JSON.stringify(apiData)
-		};
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(apiData)
+        };
 
-		fetch(getAllMyDraftReportsEndpoint, requestOptions)
-			.then(async (response) => {
-				const isJson = response.headers.get("content-type")?.includes("application/json");
+        fetch(getAllMyDraftReportsEndpoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers.get("content-type")?.includes("application/json");
 
-				const data = isJson && (await response.json());
-
-
-
-				changeDraft(await data);
-				// console.log(data[0].status);
-
-				// check for error response
-				if (!response.ok) {
-					// error
-				}
-			})
-			.catch(() => {
-				// console.log("Error Getting History");
-			});
-	};
-
-	getHistory();
-
-	// #######################################################################
-
-
-	//EXTRACTING REQUIRED DATA
-	let newDraft = draft.filter(function(data){
-		return data.status === "DRAFT";
-	}).map(function(data){
-		return data;
-	})
-
-	// if(newDraft.length !== 0){
-	// 	console.log("new draft is not empty");
-	// }
+                const data = isJson && (await response.json());
 
 
 
-	return (
-		<div>
-			{/* Api response comes here */}
-			<div className=" mt-16 p-3 border-l border-gray-200">
-				<div className=" mt-4">
-					<h1 className="text-3xl">Drafts</h1>
+                changeDraft(await data);
+                // console.log(data[0].status);
 
-					<div className="mt-4 flex flex-row flex-wrap justify-center">
-						<div className="mt-4 flex flex-row flex-wrap justify-center">
-							{newDraft.map((data) => (
-								<div
-									className="m-4 w-auto h-auto bg-gray-400 hover:bg-gray-300 rounded-md flex flex-col p-2"
-									key={data.reportID}
-								>
-									<DraftCard data={data} />
-								</div>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                // check for error response
+                if (!response.ok) {
+                    // error
+                }
+            })
+            .catch(() => {
+                // console.log("Error Getting History");
+            });
+    };
+
+    getHistory();
+
+    // #######################################################################
+
+
+    //EXTRACTING REQUIRED DATA
+    let newDraft = draft.filter(function (data) {
+        return data.status === "DRAFT";
+    }).map(function (data) {
+        return data;
+    })
+
+    // if(newDraft.length !== 0){
+    // 	console.log("new draft is not empty");
+    // }
+
+
+
+    return (
+        <div>
+            {/* Api response comes here */}
+            <div className=" mt-16 p-3 border-l border-gray-200">
+                <div className=" mt-4">
+                    <h1 className="text-3xl">Drafts</h1>
+
+                    <div className="mt-4 flex flex-row flex-wrap justify-center">
+                        <div className="mt-4 flex flex-row flex-wrap justify-center">
+                            {newDraft.map((data) => (
+                                <div
+                                    className="m-4 w-auto h-auto bg-gray-400 hover:bg-gray-300 rounded-md flex flex-col p-2"
+                                    key={data.reportID}
+                                >
+                                    <DraftCard data={data} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Drafts;
--- client/src/components/ViewHistory/ViewHistory.tsx
+++ client/src/components/ViewHistory/ViewHistory.tsx
@@ -7,207 +7,207 @@ import HomeTweet from "../HomeTweet/HomeTweet";
 import link from "../../resources/links.json";
 
 function ViewHistory() {
-	// console.log(localStorage.getItem("resultSetId"));
+    // console.log(localStorage.getItem("resultSetId"));
 
-	const [searchPhrase, changePhrase] = useState("");
-	const [sort, changeSort] = useState("");
-	const [filter, changeFilter] = useState("");
-	const [date, changeDate] = useState("");
-	const [tweets, changeTweets] = useState<any[]>([]);
-	const [genReport, changeGenReport] = useState("");
-	const [clicked, changeClicked] = useState(false);
+    const [searchPhrase, changePhrase] = useState("");
+    const [sort, changeSort] = useState("");
+    const [filter, changeFilter] = useState("");
+    const [date, changeDate] = useState("");
+    const [tweets, changeTweets] = useState<any[]>([]);
+    const [genReport, changeGenReport] = useState("");
+    const [clicked, changeClicked] = useState(false);
 
-	// ################ API FOR GET RESULT SET REPORT ###########################
+    // ################ API FOR GET RESULT SET REPORT ###########################
 
-	let getResultSetEndpoint = String(link.endpointLink);
-	getResultSetEndpoint += "getResultSet";
+    let getResultSetEndpoint = process.env.NODE_ENV === 'development' ? String(link.localhostLink) : String(link.serverLink);
+    getResultSetEndpoint += "getResultSet";
 
-	// using localhost
-	// const getResultSetEndpoint = "http://localhost:4000/dev/getResultSet";
+    // using localhost
+    // const getResultSetEndpoint = "http://localhost:4000/dev/getResultSet";
 
-	const getResultSet = async () => {
-		const resultSetData = {
-			apiKey: localStorage.getItem("loggedUserApi"),
-			resultSetID: localStorage.getItem("resultSetId")
-		};
+    const getResultSet = async () => {
+        const resultSetData = {
+            apiKey: localStorage.getItem("loggedUserApi"),
+            resultSetID: localStorage.getItem("resultSetId")
+        };
 
-		const requestOptions = {
-			method: "POST",
-			body: JSON.stringify(resultSetData)
-		};
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(resultSetData)
+        };
 
-		fetch(getResultSetEndpoint, requestOptions)
-			.then(async (response) => {
-				const isJson = response.headers.get("content-type")?.includes("application/json");
+        fetch(getResultSetEndpoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers.get("content-type")?.includes("application/json");
 
-				const data = isJson && (await response.json());
-				// console.log(data.resultSet);
+                const data = isJson && (await response.json());
+                // console.log(data.resultSet);
 
-				if (!response.ok) {
-					// error
+                if (!response.ok) {
+                    // error
 
-					return;
-				}
+                    return;
+                }
 
-				// changeDate(await data.Report.dateCreated.substring(0, 10));
-				changePhrase(await data.resultSet.searchPhrase);
-				changeDate(await data.resultSet.dateCreated.substring(0, 16));
-				changeSort(await data.resultSet.sortOption);
-				changeFilter(await data.resultSet.filterOption);
-				changeTweets(await data.Tweets);
-			})
-			.catch(() => {
-				// console.log("Error Generating Report");
-			});
-	};
+                // changeDate(await data.Report.dateCreated.substring(0, 10));
+                changePhrase(await data.resultSet.searchPhrase);
+                changeDate(await data.resultSet.dateCreated.substring(0, 16));
+                changeSort(await data.resultSet.sortOption);
+                changeFilter(await data.resultSet.filterOption);
+                changeTweets(await data.Tweets);
+            })
+            .catch(() => {
+                // console.log("Error Generating Report");
+            });
+    };
 
-	getResultSet();
-	// ###################################################################
+    getResultSet();
+    // ###################################################################
 
-	// ################ API FOR GENERATE REPORT ###########################
+    // ################ API FOR GENERATE REPORT ###########################
 
-	let genReportEndpoint = String(link.endpointLink);
-	genReportEndpoint += "generateReport";
+    let genReportEndpoint = process.env.NODE_ENV === 'development' ? String(link.localhostLink) : String(link.serverLink);
+    genReportEndpoint += "generateReport";
 
-	// using localhost
-	// const genReportEndpoint = "http://localhost:4000/dev/generateReport";
+    // using localhost
+    // const genReportEndpoint = "http://localhost:4000/dev/generateReport";
 
-	const genRep = async () => {
-		const searchData = {
-			apiKey: localStorage.getItem("loggedUserApi"),
-			author: localStorage.getItem("loggedUserName"),
-			resultSetID: localStorage.getItem("resultSetId")
-		};
+    const genRep = async () => {
+        const searchData = {
+            apiKey: localStorage.getItem("loggedUserApi"),
+            author: localStorage.getItem("loggedUserName"),
+            resultSetID: localStorage.getItem("resultSetId")
+        };
 
-		const requestOptions = {
-			method: "POST",
-			body: JSON.stringify(searchData)
-		};
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(searchData)
+        };
 
-		fetch(genReportEndpoint, requestOptions)
-			.then(async (response) => {
-				const isJson = response.headers.get("content-type")?.includes("application/json");
+        fetch(genReportEndpoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers.get("content-type")?.includes("application/json");
 
-				const data = isJson && (await response.json());
+                const data = isJson && (await response.json());
 
-				if (!response.ok) {
-					// error
+                if (!response.ok) {
+                    // error
 
-					return;
-				}
+                    return;
+                }
 
-				changeDate(await data.Report.dateCreated.substring(0, 10));
-				changeGenReport(data.Report.reportID);
-				changeClicked(true);
-			})
-			.catch(() => {
-				// console.log("Error Generating Report");
-			});
-	};
+                changeDate(await data.Report.dateCreated.substring(0, 10));
+                changeGenReport(data.Report.reportID);
+                changeClicked(true);
+            })
+            .catch(() => {
+                // console.log("Error Generating Report");
+            });
+    };
 
-	// ###################################################################
+    // ###################################################################
 
-	// tweet options
-	const tweetOptions = [];
+    // tweet options
+    const tweetOptions = [];
 
-	for (let index = 1; index <= 100; index++) {
-		tweetOptions.push(<option key={index.toString()}>{index}</option>);
-	}
+    for (let index = 1; index <= 100; index++) {
+        tweetOptions.push(<option key={index.toString()}>{index}</option>);
+    }
 
-	// processing api response
-	const apiResponse = [<div key="begining div" />];
+    // processing api response
+    const apiResponse = [<div key="begining div" />];
 
-	tweets.map((data) =>
-		apiResponse.push(
-			<div key={data.tweetId}>
-				<HomeTweet tweetData={data} />
-			</div>
-		)
-	);
+    tweets.map((data) =>
+        apiResponse.push(
+            <div key={data.tweetId}>
+                <HomeTweet tweetData={data} />
+            </div>
+        )
+    );
 
-	// let ind = 0;
+    // let ind = 0;
 
-	const viewGenReport = () => {
-		// if (enteredSearch !== "") {
-		localStorage.setItem("draftReportId", genReport);
-		// }
-	};
+    const viewGenReport = () => {
+        // if (enteredSearch !== "") {
+        localStorage.setItem("draftReportId", genReport);
+        // }
+    };
 
-	return (
-		<div>
-			{!clicked && (
-				<div>
-					<div className="flex flex-col flex-wrap justify-around pt-3 pb-3 ">
-						{/*  */}
-						<h1 className="text-3xl mt-12">{searchPhrase}</h1>
+    return (
+        <div>
+            {!clicked && (
+                <div>
+                    <div className="flex flex-col flex-wrap justify-around pt-3 pb-3 ">
+                        {/*  */}
+                        <h1 className="text-3xl mt-12">{searchPhrase}</h1>
 
-						<div className="flex flex-col mt-4">
-							<div className="flex flex-row flex-wrap w-auto font-light text-sm italic">
-								<p>Date searched:</p> &nbsp;
-								<p>{date}</p>
-							</div>
+                        <div className="flex flex-col mt-4">
+                            <div className="flex flex-row flex-wrap w-auto font-light text-sm italic">
+                                <p>Date searched:</p> &nbsp;
+                                <p>{date}</p>
+                            </div>
 
-							<div className="flex flex-row">
-								<div className="flex flex-row flex-wrap w-auto text-sm">
-									<p className="">Filtered by:</p> &nbsp;
-									<p>{filter}</p>
-								</div>
+                            <div className="flex flex-row">
+                                <div className="flex flex-row flex-wrap w-auto text-sm">
+                                    <p className="">Filtered by:</p> &nbsp;
+                                    <p>{filter}</p>
+                                </div>
 
-								{/* this is for the sorting options */}
-								<div className="flex flex-row flex-wrap w-auto text-sm ml-3">
-									<p className="">Sorted:</p> &nbsp;
-									<p>{sort}</p>
-								</div>
-							</div>
-							{/* this is for the Fitlering options */}
-						</div>
-					</div>
+                                {/* this is for the sorting options */}
+                                <div className="flex flex-row flex-wrap w-auto text-sm ml-3">
+                                    <p className="">Sorted:</p> &nbsp;
+                                    <p>{sort}</p>
+                                </div>
+                            </div>
+                            {/* this is for the Fitlering options */}
+                        </div>
+                    </div>
 
-					<div data-testid="result" className="flex flex-col">
-						{apiResponse}
-						<br />
-						<div className="flex flex-row w-full justify-center pt-3">
-							<button
-								data-testid="btn-generate"
-								type="submit"
-								className="button w-3/4 text-lg p-0.5"
-								onClick={genRep}
-							>
-								Generate Report
-							</button>
-						</div>
-					</div>
-				</div>
-			)}
+                    <div data-testid="result" className="flex flex-col">
+                        {apiResponse}
+                        <br />
+                        <div className="flex flex-row w-full justify-center pt-3">
+                            <button
+                                data-testid="btn-generate"
+                                type="submit"
+                                className="button w-3/4 text-lg p-0.5"
+                                onClick={genRep}
+                            >
+                                Generate Report
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
 
-			{clicked && (
-				<div className="mt-4 flex flex-col flex-wrap justify-center">
-					<h1 className="text-2xl">Newly created report</h1>
-					<Link to="/genReport">
-						<div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
-							<div className="">
-								<button
-									data-testid="btn-report"
-									type="submit"
-									onClick={viewGenReport}
-								>
-									<p className="font-bold">{searchPhrase}</p>
-								</button>
-							</div>
-							<div className="mt-2">
-								<p className="italic text-xs">
-									{localStorage.getItem("loggedUserName")}
-								</p>
-							</div>
-							<div className="">
-								<p className="italic text-xs">{date}</p>
-							</div>
-						</div>
-					</Link>
-				</div>
-			)}
-		</div>
-	);
+            {clicked && (
+                <div className="mt-4 flex flex-col flex-wrap justify-center">
+                    <h1 className="text-2xl">Newly created report</h1>
+                    <Link to="/genReport">
+                        <div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
+                            <div className="">
+                                <button
+                                    data-testid="btn-report"
+                                    type="submit"
+                                    onClick={viewGenReport}
+                                >
+                                    <p className="font-bold">{searchPhrase}</p>
+                                </button>
+                            </div>
+                            <div className="mt-2">
+                                <p className="italic text-xs">
+                                    {localStorage.getItem("loggedUserName")}
+                                </p>
+                            </div>
+                            <div className="">
+                                <p className="italic text-xs">{date}</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default ViewHistory;
