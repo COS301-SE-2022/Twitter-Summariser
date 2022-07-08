faf67be8ba6cf7bfb77f4e3493f5d12930d603e3
Tlholo Koma <tlholokoma@gmail.com>
2022-07-08 02:21:26 +0200

    :white_check_mark: Add unit test for report block

---
+++ src/services/__tests__/reportBlock.service.spec.ts
@@ -0,0 +1,52 @@
+import ReportBlock from "@model/reportBlock/reportBlock.model";
+import ReportBlockService from "..";
+
+import { DocumentClient, awsSdkPromiseResponse } from "../../__mocks__/aws-sdk";
+
+const db = new DocumentClient();
+
+describe("reportBlock.service", () => {
+    beforeAll(() => {
+        jest.useFakeTimers("modern");
+        jest.setSystemTime(new Date(2022, 1, 1));
+    });
+
+    beforeEach(() => {
+        awsSdkPromiseResponse.mockReset();
+    });
+
+
+    describe("getReportBlock", () => {
+        test("Get Report Block",async () => {
+            const addedReportBlock: ReportBlock = {
+                reportBlockID: "9000",
+                reportID: "1111",
+                blockType: "RICHTEXT",
+                position: 0,
+                richText: "Testing"
+            };
+
+            awsSdkPromiseResponse.mockReturnValueOnce(Promise.resolve({ Item: addedReportBlock }));
+
+            const reportBlock = await ReportBlockService.reportBlockService.getReportBlock("9000");
+
+            expect(db.get).toHaveBeenCalledWith({
+                TableName: "ReportBlockTable",
+                Key: { id: "9000" }
+            });
+
+            const expected = {
+                reportBlockID: "9000",
+                reportID: "1111",
+                blockType: "RICHTEXT",
+                position: 0,
+                richText: "Testing"
+            };
+
+            expect(reportBlock).toEqual(expected);
+
+        })
+    })
+
+
+})
