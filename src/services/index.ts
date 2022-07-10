54e714ed97abc4082b601c5ffdc0a6ae4386a3c8
Tlholo Koma <tlholokoma@gmail.com>
2022-07-10 17:57:05 +0200

    :ambulance: Call default tweet service constructor.

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,16 +5,19 @@ import ReportBlockService from "./reportBlock.service";
 import ResultSetService from "./resultSet.service";
 import TextStyleService from "./textStyles.service";
 import PermissionService from "./permission.service";
+import TweetService from "./tweet.service";
 
 const reportService = new ReportService(dynamoDBClient());
 const creatorService = new CreatorService(dynamoDBClient());
 const resultSetServices = new ResultSetService(dynamoDBClient());
 const reportBlockService = new ReportBlockService(dynamoDBClient());
 const textStyleService = new TextStyleService(dynamoDBClient());
+const tweetService= new TweetService();
 const permissionService = new PermissionService(dynamoDBClient());
 
 export default {
 	creatorService,
+	tweetService,
 	resultSetServices,
 	reportService,
 	reportBlockService,
