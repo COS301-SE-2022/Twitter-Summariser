eb5f4f7a76a0da74902d57a5fc20d42e70cab233
Zenthon <Zenthon@users.noreply.github.com>
2022-07-11 13:15:59 +0000

    🎨(CICD): All code has been formatted!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,58 +6,58 @@ import "./index.css";
 
 // main Application component in which different page sub-components will be contained
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    			const [signupPage, setSignupPage] = useState(false);
-    const [userApi, changeUserApi] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [signupPage, setSignupPage] = useState(false);
+	const [userApi, changeUserApi] = useState("");
 
-    useEffect(() => {
-        const storageUserLoggedInInformation = localStorage.getItem("key");
-        if (storageUserLoggedInInformation) {
-            setIsLoggedIn(true);
-        }
-    }, []);
+	useEffect(() => {
+		const storageUserLoggedInInformation = localStorage.getItem("key");
+		if (storageUserLoggedInInformation) {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
-    // retrieving user login details
-    const loginHandler = (props: any) => {
-        if (props.login === "true") {
-            			localStorage.setItem("key", props.apiKey);
-            localStorage.setItem("username", props.username);
-            localStorage.setItem("email", props.email);
-            setIsLoggedIn(true);
-            setSignupPage(false);
-            changeUserApi(props.apiKey);
-        }
-    };
+	// retrieving user login details
+	const loginHandler = (props: any) => {
+		if (props.login === "true") {
+			localStorage.setItem("key", props.apiKey);
+			localStorage.setItem("username", props.username);
+			localStorage.setItem("email", props.email);
+			setIsLoggedIn(true);
+			setSignupPage(false);
+			changeUserApi(props.apiKey);
+		}
+	};
 
-    const signUpPage = () => {
-        setIsLoggedIn(false);
-        setSignupPage(true);
-    };
+	const signUpPage = () => {
+		setIsLoggedIn(false);
+		setSignupPage(true);
+	};
 
-    const logInPage = () => {
-        localStorage.clear();
-        setIsLoggedIn(false);
-        setSignupPage(false);
-    };
+	const logInPage = () => {
+		localStorage.clear();
+		setIsLoggedIn(false);
+		setSignupPage(false);
+	};
 
-    const readyToLog = () => {
-        				localStorage.setItem("newUser", "true");
-        setSignupPage(false);
-    };
+	const readyToLog = () => {
+		localStorage.setItem("newUser", "true");
+		setSignupPage(false);
+	};
 
-    return (
-        			<div className="">
-            {!localStorage.getItem("key") && !signupPage && (
-                <Login userLoginDetails={loginHandler} takeToSignupPage={signUpPage} />
-            )}
+	return (
+		<div className="">
+			{!localStorage.getItem("key") && !signupPage && (
+				<Login userLoginDetails={loginHandler} takeToSignupPage={signUpPage} />
+			)}
 
-            {/* Signup */}
-            {signupPage && <Signup takeToSigninPage={logInPage} readyToLogIN={readyToLog} />}
+			{/* Signup */}
+			{signupPage && <Signup takeToSigninPage={logInPage} readyToLogIN={readyToLog} />}
 
-            {/* Entry here based on Signup and Login decision  */}
-            {isLoggedIn && <Landing userAPI={userApi} takeToSigninPage={logInPage} />}
-        </div>
-    );
+			{/* Entry here based on Signup and Login decision  */}
+			{isLoggedIn && <Landing userAPI={userApi} takeToSigninPage={logInPage} />}
+		</div>
+	);
 }
 
 export default App;
