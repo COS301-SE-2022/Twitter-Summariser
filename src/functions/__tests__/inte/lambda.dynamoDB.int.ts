32165a40ac5a7994300e138ae645cda732f519e4
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-12 05:34:16 +0200

    :zap::sparkles: Added Lambda function invoker

--- src/functions/__tests__/inte/lambda.dynamoDB.int.ts
+++ src/functions/__tests__/inte/lambda.dynamoDB.int.ts
@@ -1,6 +1,7 @@
 import { dynamoDB, lambda } from "@functions/__tests__/inte/resources";
 
 describe(" ", async () => {
+	// Creation of table
 	await dynamoDB
 		.put({
 			TableName: "Users",
@@ -11,8 +12,23 @@ describe(" ", async () => {
 		})
 		.promise();
 
-    await dynamoDB.delete({
-        TableName: "Users",
-        Key: { HashKey: "userID"}
-    }).promise();
+	// Invoking Lambda function
+	await lambda
+		.invoke({
+			FunctionName: "createAdminFile",
+			Payload: JSON.stringify({
+				userID: "userId",
+				filename: "sample.txt",
+				content: "ok"
+			})
+		})
+		.promise();
+
+	// Deleting table
+	await dynamoDB
+		.delete({
+			TableName: "Users",
+			Key: { HashKey: "userID" }
+		})
+		.promise();
 });
