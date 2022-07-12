1394524306982e11b846d546c4ab44efce1783f8
Tlholo Koma <tlholokoma@gmail.com>
2022-07-12 13:33:49 +0200

    :white_check_mark: Add unit test for text styles service

---
+++ src/services/__tests__/textStyles.service.spec.ts
@@ -0,0 +1,56 @@
+import TextStyle from "@model/textStyles/textStyles.model";
+import TextStyleService from "..";
+
+import { DocumentClient, awsSdkPromiseResponse } from "../../__mocks__/aws-sdk";
+
+const db = new DocumentClient();
+
+describe("textStyles.service", () => {
+    beforeAll(() => {
+        jest.useFakeTimers("modern");
+        jest.setSystemTime(new Date(2022, 1, 1));
+    });
+
+    beforeEach(() => {
+        awsSdkPromiseResponse.mockReset();
+    });
+
+    describe("getTextStyle", () => {
+        test("Get Text Styles",async () => {
+            const addedTextStyles: TextStyle = {
+                textStylesID: "4111",
+                reportBlockID: "700",
+                align: "center",
+                bold: "no",
+                colour: "black",
+                italic: "yes",
+                size: "12pt"
+            };
+
+            awsSdkPromiseResponse.mockReturnValueOnce(Promise.resolve({ Items: addedTextStyles }));
+
+            const textStyle = await TextStyleService.textStyleService.getStyle("700");
+
+            expect(db.query).toHaveBeenCalledWith({
+                TableName: "TextStylesTable",
+                IndexName: "textStylesIndex",
+                KeyConditionExpression: "reportBlockID = :reportBlockID",
+                ExpressionAttributeValues: {
+                    ":reportBlockID": "700"
+                }
+            });
+
+            const expected = {
+                textStylesID: "4111",
+                reportBlockID: "700",
+                align: "center",
+                bold: "no",
+                colour: "black",
+                italic: "yes",
+                size: "12pt"
+            };
+
+            expect(textStyle).toEqual(expected);
+        })
+    })
+})
