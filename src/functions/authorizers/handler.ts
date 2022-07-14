41faadb55141527d01a05dd111afd0a21165c67d
Zenthon <u20536951@tuks.co.za>
2022-07-14 19:28:14 +0200

    🔥 (CICD): Minor changes

--- src/functions/authorizers/handler.ts
+++ src/functions/authorizers/handler.ts
@@ -1,27 +1,6 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyCallbackV2 } from "aws-lambda";
-import { middyfy } from "@libs/lambda";
-import { header, statusCodes } from "@functions/resources/APIresponse";
 import jwt from "jsonwebtoken";
 
-// export const verifyJWT = middyfy(async (event: APIGatewayProxyEventV2, callback: APIGatewayProxyCallbackV2): Promise<void> => {
-//     // const jwt_token = event.headers.authorization;
-
-
-//     return callback(null, { statusCode: statusCodes.unauthorized, headers: header, body: JSON.stringify({ message: "Lol" }) });
-
-
-
-
-
-//     // const secret = process.env.ACCESS_TOKEN_SECRET;
-//     // // const decoded = jwt.verify(jwt_token, secret);
-
-//     // if (decoded && decoded.email)
-//     //     return callback(null, authorizationResponse(decoded.email, "Allow"));
-//     // return callback(null, authorizationResponse(decoded.email, "Deny"));
-// })
-
-export const verifyJWT = (event, context, callback) => {
+export const verifyJWT = async (event, callback) => {
     const methodARN = event.methodArn;
     try {
         const jwt_token = event.authorizationToken;
@@ -32,22 +11,15 @@ export const verifyJWT = (event, context, callback) => {
 
         const secret = process.env.ACCESS_TOKEN_SECRET;
         const decoded = jwt.verify(jwt_token, secret);
-        // const jwt_token_payload = JSON.parse(jwt_token.split(".")[1]);
-        // console.log(jwt_token_payload);
-
-
 
         if (decoded && decoded.email)
             return callback(null, authorizationResponse(decoded.email, "Allow", methodARN));
         return callback(null, authorizationResponse(decoded.email, "Deny", methodARN));
 
     } catch (e) {
-        console.log(e);
-
         return callback(null, authorizationResponse("ExpiredToken", "Deny", methodARN));
     }
 }
-
 
 function authorizationResponse(principalId: string, effect: string, arn: string): object {
     return {
