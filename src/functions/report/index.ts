8b94a404bf78d276672916255753d3f37447c6e9
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-14 20:32:13 +0200

    :truck: added function code

--- src/functions/report/index.ts
+++ src/functions/report/index.ts
@@ -135,21 +135,6 @@ export const cloneReport = {
 	]
 };
 
-// Adding a custom tweet
-export const addCustomTweet = {
-	handler: `${handlerPath(__dirname)}/handler.addCustomTweet`,
-	description: "A function that adds customer tweets.",
-	events: [
-		{
-			http: {
-				method: "post",
-				path: "addCustomTweet/",
-				cors: true
-			}
-		}
-	]
-};
-
 // Deleting a result report
 export const deleteReport = {
 	handler: `${handlerPath(__dirname)}/handler.deleteReport`,
