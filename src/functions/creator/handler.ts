35d82305ecc5517e20483a77a19b7f2fb145677a
Zenthon <u20536951@tuks.co.za>
2022-07-16 11:39:54 +0200

    ♻️ 👽️(API): Added error handling & new logic

--- src/functions/creator/handler.ts
+++ src/functions/creator/handler.ts
@@ -9,151 +9,189 @@ import CreatorServices from "../../services";
 dotenv.config();
 
 export const getAllCreators = middyfy(async (): Promise<APIGatewayProxyResultV2> => {
-	const creators = await CreatorServices.creatorService.getAllCreators();
-	try {
-		return {
-			statusCode: statusCodes.Successful,
-			headers: header,
-			body: JSON.stringify(creators)
-		};
-	} catch (error) {
-		return {
-			statusCode: statusCodes.internalError,
-			headers: header,
-			body: JSON.stringify(error)
-		};
-	}
+    const creators = await CreatorServices.creatorService.getAllCreators();
+    try {
+        return {
+            statusCode: statusCodes.Successful,
+            headers: header,
+            body: JSON.stringify(creators)
+        };
+    } catch (error) {
+        return {
+            statusCode: statusCodes.internalError,
+            headers: header,
+            body: JSON.stringify(error)
+        };
+    }
 });
 
 export const addCreator = middyfy(
-	async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-		let apiKey: string;
-		const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		apiKey = "";
+    async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+        const params = JSON.parse(event.body);
+        if (Object.keys(params).length !== 3) {
+            return {
+                statusCode: statusCodes.badRequest,
+                headers: header,
+                body: JSON.stringify({ message: "Please provide all/only the required fields" })
+            }
+        }
 
-		for (let i = 0; i < 15; i++) {
-			apiKey += characters.charAt(Math.floor(Math.random() * characters.length) + 0);
-		}
+        if (!params.email || !params.password || !params.username) {
+            return {
+                statusCode: statusCodes.badRequest,
+                headers: header,
+                body: JSON.stringify({ error: "Missing username or password" })
+            };
+        }
 
-		const params = JSON.parse(event.body);
+        const creatorsArray = await CreatorServices.creatorService.getAllCreators();
+        for (const creator of creatorsArray) {
+            if (creator.email === params.email) {
+                return {
+                    statusCode: statusCodes.badRequest,
+                    headers: header,
+                    body: JSON.stringify({ error: "Email already exists" })
+                };
+            }
+            if (creator.username === params.username) {
+                return {
+                    statusCode: statusCodes.badRequest,
+                    headers: header,
+                    body: JSON.stringify({ error: "Username already exists" })
+                };
+            }
+        }
 
-		const hashedPass = bcrypt.hashSync(params.password, 10);
+        try {
+            let apiKey = "";
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const hashedPass = bcrypt.hashSync(params.password, 10);
 
-		try {
-			const creator = await CreatorServices.creatorService.addCreator({
-				apiKey,
-				email: params.email,
-				username: params.username,
-				password: hashedPass,
-				dateOfBirth: params.dateOfBirth,
-				dateRegistered: new Date().toISOString()
-			});
 
-			const response = {
-				apiKey: creator.apiKey,
-				email: creator.email,
-				username: creator.username
-			};
+            for (let i = 0; i < 15; i++)
+                apiKey += characters.charAt(Math.floor(Math.random() * characters.length) + 0);
 
-			return {
-				statusCode: statusCodes.Successful,
-				headers: header,
-				body: JSON.stringify(response)
-			};
-		} catch (e) {
-			return {
-				statusCode: statusCodes.internalError,
-				headers: header,
-				body: JSON.stringify(e)
-			};
-		}
-	}
+
+            const creator = await CreatorServices.creatorService.addCreator({
+                apiKey,
+                email: params.email,
+                username: params.username,
+                password: hashedPass,
+                dateRegistered: new Date().toISOString()
+            });
+
+            const response = {
+                apiKey: creator.apiKey,
+                email: creator.email,
+                username: creator.username
+            };
+
+            return {
+                statusCode: statusCodes.Successful,
+                headers: header,
+                body: JSON.stringify(response)
+            };
+        } catch (e) {
+            return {
+                statusCode: statusCodes.internalError,
+                headers: header,
+                body: JSON.stringify(e)
+            };
+        }
+    }
 );
 
 export const loginCreator = middyfy(
-	async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-		const params = JSON.parse(event.body);
+    async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+        const params = JSON.parse(event.body);
 
-		if (!params.email || !params.password) {
-			return {
-				statusCode: statusCodes.badRequest,
-				headers: header,
-				body: JSON.stringify({ error: "Missing username or password" })
-			};
-		}
-		try {
-			const creator = await CreatorServices.creatorService.getCreator(params.email);
+        if (Object.keys(params).length !== 2) {
+            return {
+                statusCode: statusCodes.badRequest,
+                headers: header,
+                body: JSON.stringify({ message: "Please provide all/only the required fields" })
+            }
+        }
 
-			if (creator === undefined) {
-				return {
-					statusCode: statusCodes.unauthorized,
-					headers: header,
-					body: JSON.stringify(`creator ${params.email} not found`)
-				};
-			}
-			if ((await bcrypt.compare(params.password, creator.password)) !== true) {
-				return {
-					statusCode: statusCodes.unauthorized,
-					headers: header,
-					body: JSON.stringify(`Invalid password for user ${params.email}`)
-				};
-			}
-			// Create the Access Token
-			const accessToken = jwt.sign(
-				{
-					email: creator.email,
-					username: creator.username,
-					apiKey: creator.apiKey
-				},
-				process.env.ACCESS_TOKEN_SECRET,
-				{
-					expiresIn: "1m"
-				}
-			);
+        if (!params.email || !params.password) {
+            return {
+                statusCode: statusCodes.badRequest,
+                headers: header,
+                body: JSON.stringify({ error: "Missing username or password" })
+            };
+        }
+        try {
+            const creator = await CreatorServices.creatorService.getCreator(params.email);
 
-			// Create the Refresh Token
-			const refreshToken = jwt.sign(
-				{
-					email: creator.email,
-					username: creator.username,
-					apiKey: creator.apiKey
-				},
-				process.env.REFRESH_TOKEN_SECRET,
-				{
-					expiresIn: "1d"
-				}
-			);
+            if (creator === undefined) {
+                return {
+                    statusCode: statusCodes.unauthorized,
+                    headers: header,
+                    body: JSON.stringify(`creator ${params.email} not found`)
+                };
+            }
+            if ((await bcrypt.compare(params.password, creator.password)) !== true) {
+                return {
+                    statusCode: statusCodes.unauthorized,
+                    headers: header,
+                    body: JSON.stringify(`Invalid password for user ${params.email}`)
+                };
+            }
+            // Create the Access Token
+            const accessToken = jwt.sign(
+                {
+                    email: creator.email,
+                    username: creator.username,
+                    apiKey: creator.apiKey
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: "1m"
+                }
+            );
 
-			// Update Creator in DB
-			const isCreatorUpdated = await CreatorServices.creatorService.updateCreator(
-				creator.email,
-				refreshToken
-			);
+            // Create the Refresh Token
+            const refreshToken = jwt.sign(
+                {
+                    email: creator.email,
+                    username: creator.username,
+                    apiKey: creator.apiKey
+                },
+                process.env.REFRESH_TOKEN_SECRET,
+                {
+                    expiresIn: "1d"
+                }
+            );
 
-			if (isCreatorUpdated === true) {
-				const cookieString = `refreshToken=${refreshToken}; HttpOnly; max-age=${
-					24 * 60 * 60 * 1000
-				}`;
-				return {
-					statusCode: statusCodes.Successful,
-					headers: {
-						...header,
-						"Set-Cookie": cookieString
-					},
-					body: JSON.stringify({ accessToken })
-				};
-			}
-			return {
-				statusCode: statusCodes.internalError,
-				headers: header,
-				body: JSON.stringify(`Error updating creator ${creator.email}; ${isCreatorUpdated}`)
-			};
-		} catch (e) {
-			return {
-				statusCode: statusCodes.internalError,
-				headers: header,
-				body: JSON.stringify({ message: e.message })
-			};
-		}
-	}
+            // Update Creator in DB
+            const isCreatorUpdated = await CreatorServices.creatorService.updateCreator(
+                creator.email,
+                refreshToken
+            );
+
+            if (isCreatorUpdated === true) {
+                const cookieString = `refreshToken=${refreshToken}; HttpOnly; max-age=${24 * 60 * 60 * 1000
+                    }`;
+                return {
+                    statusCode: statusCodes.Successful,
+                    headers: {
+                        ...header,
+                        "Set-Cookie": cookieString
+                    },
+                    body: JSON.stringify({ accessToken })
+                };
+            }
+            return {
+                statusCode: statusCodes.internalError,
+                headers: header,
+                body: JSON.stringify(`Error updating creator ${creator.email}; ${isCreatorUpdated}`)
+            };
+        } catch (e) {
+            return {
+                statusCode: statusCodes.internalError,
+                headers: header,
+                body: JSON.stringify({ message: e.message })
+            };
+        }
+    }
 );
--- src/services/creator.service.ts
+++ src/services/creator.service.ts
@@ -2,85 +2,81 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import Creator from "@model/creator/creator.model";
 
 export default class CreatorService {
-	private TableName = "CreatorTable";
+    private TableName = "CreatorTable";
 
-	constructor(private docClient: DocumentClient) {}
+    constructor(private docClient: DocumentClient) { }
 
-	async getAllCreators(): Promise<Creator[]> {
-		const creator = await this.docClient
-			.scan({
-				TableName: this.TableName
-			})
-			.promise();
-		return creator.Items as Creator[];
-	}
+    async getAllCreators(): Promise<Creator[]> {
+        const creator = await this.docClient
+            .scan({
+                TableName: this.TableName
+            })
+            .promise();
+        return creator.Items as Creator[];
+    }
 
-	async addCreator(creator: Creator): Promise<Creator> {
-		if (!creator) {
-			throw new Error("no new creator provided");
-		}
-		await this.docClient
-			.put({
-				TableName: this.TableName,
-				Item: creator,
-				ConditionExpression: "email <> :email",
-				ExpressionAttributeValues: {
-					":email": creator.email
-				}
-			})
-			.promise();
-		return creator as Creator;
-	}
+    async addCreator(creator: Creator): Promise<Creator> {
+        if (!creator) {
+            throw new Error("no new creator provided");
+        }
+        await this.docClient
+            .put({
+                TableName: this.TableName,
+                Item: creator,
+                ConditionExpression: "email <> :email",
+                ExpressionAttributeValues: {
+                    ":email": creator.email
+                }
+            })
+            .promise();
+        return creator as Creator;
+    }
 
-	async getCreator(email: string): Promise<Creator> {
-		const result = await this.docClient
-			.get({
-				TableName: this.TableName,
-				Key: {
-					email
-				}
-			})
-			.promise();
+    async getCreator(email: string): Promise<Creator> {
+        const result = await this.docClient
+            .get({
+                TableName: this.TableName,
+                Key: {
+                    email
+                }
+            })
+            .promise();
 
-		return result.Item as Creator;
-	}
+        return result.Item as Creator;
+    }
 
-	async getCreatorByKey(key: string): Promise<Creator> {
-		const creator = await this.docClient
-			.query({
-				TableName: this.TableName,
-				IndexName: "gsiIndex",
-				KeyConditionExpression: "apiKey = :key",
-				ExpressionAttributeValues: {
-					":key": key
-				}
-			})
-			.promise();
+    async getCreatorByKey(key: string): Promise<Creator> {
+        const creator = await this.docClient
+            .query({
+                TableName: this.TableName,
+                IndexName: "gsiIndex",
+                KeyConditionExpression: "apiKey = :key",
+                ExpressionAttributeValues: {
+                    ":key": key
+                }
+            })
+            .promise();
 
-		return creator.Items[0] as Creator;
-	}
+        return creator.Items[0] as Creator;
+    }
 
-	async updateCreator(email: string, token: string): Promise<Boolean> {
-		return await this.docClient
-			.update({
-				TableName: this.TableName,
-				Key: {
-					email
-				},
-				UpdateExpression: "set #RefreshAccessToken = :RefreshAccessToken",
-				ExpressionAttributeNames: {
-					"#RefreshAccessToken": "RefreshAccessToken"
-				},
-				ExpressionAttributeValues: {
-					":RefreshAccessToken": token
-				}
-			})
-			.promise()
-			.then(() => {
-				return true;
-			})
-			.catch((err) => {
-				return err;
-			});
-	}
+    async updateCreator(email: string, token: string): Promise<boolean> {
+        return this.docClient
+            .update({
+                TableName: this.TableName,
+                Key: {
+                    email
+                },
+                UpdateExpression: "set #RefreshAccessToken = :RefreshAccessToken",
+                ExpressionAttributeNames: {
+                    "#RefreshAccessToken": "RefreshAccessToken"
+                },
+                ExpressionAttributeValues: {
+                    ":RefreshAccessToken": token
+                }
+            })
+            .promise()
+            .then(() => true)
+            .catch((err) => err);
+    }
 }
