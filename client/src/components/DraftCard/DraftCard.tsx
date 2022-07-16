eb22e54ed820a3da1abd2fe8e681f2998d857948
Zenthon <u20536951@tuks.co.za>
2022-07-16 14:32:45 +0200

    🚧 ✨ 💄 (UI): Added incomplete auth to all pages

--- client/src/components/DraftCard/DraftCard.tsx
+++ client/src/components/DraftCard/DraftCard.tsx
@@ -7,129 +7,132 @@ import link from "../../resources/links.json";
 import Button from "../Button/Button";
 
 function DraftCard(props: any) {
-	// console.log(props);
+    // console.log(props);
 
-	const viewGenReport = () => {
-		if (localStorage.getItem("draftReportId")) {
-			localStorage.removeItem("draftReportId");
-			localStorage.setItem("draftReportId", props.data.reportID);
-		} else {
-			localStorage.setItem("draftReportId", props.data.reportID);
-		}
-	};
+    const viewGenReport = () => {
+        if (localStorage.getItem("draftReportId")) {
+            localStorage.removeItem("draftReportId");
+            localStorage.setItem("draftReportId", props.data.reportID);
+        } else {
+            localStorage.setItem("draftReportId", props.data.reportID);
+        }
+    };
 
-	// const iconStyle3 = { fontSize: "1.5rem", color: "red" };
+    // const iconStyle3 = { fontSize: "1.5rem", color: "red" };
 
-	// const deleteDraftHandler = (event: any) => {
-	const deleteDraftHandler = (event: any) => {
-		// event.preventDefault();
+    // const deleteDraftHandler = (event: any) => {
+    const deleteDraftHandler = (event: any) => {
+        // event.preventDefault();
 
-		const resultDetails = {
-			reportID: props.data.reportID,
-			apiKey: props.data.apiKey
-		};
+        const resultDetails = {
+            reportID: props.data.reportID,
+            apiKey: props.data.apiKey
+        };
 
-		deleteDraft(resultDetails);
-	};
+        deleteDraft(resultDetails);
+    };
 
-	// ######################### API FOR DELETING RESULT SET ###############################################
+    // ######################### API FOR DELETING RESULT SET ###############################################
 
-	let deleteDraftEndpoint =
-		process.env.NODE_ENV === "development"
-			? String(link.localhostLink)
-			: String(link.serverLink);
-	deleteDraftEndpoint += "deleteReport";
+    let deleteDraftEndpoint =
+        process.env.NODE_ENV === "development"
+            ? String(link.localhostLink)
+            : String(link.serverLink);
+    deleteDraftEndpoint += "deleteReport";
 
-	const deleteDraft = (resultInfo: any) => {
-		const requestOptions = {
-			method: "POST",
-			body: JSON.stringify(resultInfo)
-		};
+    const deleteDraft = (resultInfo: any) => {
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(resultInfo),
+            headers: {
+                "Authorization": `${localStorage.getItem("token")}`,
+            }
+        };
 
-		fetch(deleteDraftEndpoint, requestOptions)
-			.then(async (response) => {
-				const isJson = response.headers.get("content-type")?.includes("application/json");
+        fetch(deleteDraftEndpoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers.get("content-type")?.includes("application/json");
 
-				isJson && (await response.json());
+                isJson && (await response.json());
 
-				// check for error response
-				if (!response.ok) {
-					// error
+                // check for error response
+                if (!response.ok) {
+                    // error
 
-					return;
-				}
-			})
-			.catch(() => {});
-	};
+                    return;
+                }
+            })
+            .catch(() => { });
+    };
 
-	// #######################################################################
+    // #######################################################################
 
-	return (
-		<div className="p-8 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
-			<div className="flex items-center justify-center">
-				<p aria-label="Author" title="Author" className="mr-3 ">
-					<img
-						src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-						alt="avatar"
-						className="object-cover w-10 h-10 rounded-full shadow-sm"
-					/>
-				</p>
-				<div>
-					<p
-						aria-label="Author"
-						title="Author"
-						className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
-					>
-						{props.data.author}
-					</p>
-					<p className="text-sm font-medium leading-4 text-gray-600">Author</p>
-				</div>
-			</div>
-			<br></br>
+    return (
+        <div className="p-8 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
+            <div className="flex items-center justify-center">
+                <p aria-label="Author" title="Author" className="mr-3 ">
+                    <img
+                        src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                        alt="avatar"
+                        className="object-cover w-10 h-10 rounded-full shadow-sm"
+                    />
+                </p>
+                <div>
+                    <p
+                        aria-label="Author"
+                        title="Author"
+                        className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                    >
+                        {props.data.author}
+                    </p>
+                    <p className="text-sm font-medium leading-4 text-gray-600">Author</p>
+                </div>
+            </div>
+            <br></br>
 
-			<p
-				aria-label="Article"
-				title="Jingle Bells"
-				className="flex items-center justify-center inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
-			>
-				{props.data.title}
-			</p>
-			<p className="flex mb-3 text-gray-600 text-xs items-center justify-center font-semibold tracking-wide uppercase">
-				<span className=" font-semiboldtext-deep-purple-accent-400">Draft Report - </span>
-				{props.data.dateCreated.substring(4, 16)}
-			</p>
-			<div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-				<Link to="/genReport">
-					{/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
-					<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
-						<div className="text-left">
-							{/* <button data-testid="btn-report" type="submit" onClick={viewGenReport}>
+            <p
+                aria-label="Article"
+                title="Jingle Bells"
+                className="flex items-center justify-center inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+            >
+                {props.data.title}
+            </p>
+            <p className="flex mb-3 text-gray-600 text-xs items-center justify-center font-semibold tracking-wide uppercase">
+                <span className=" font-semiboldtext-deep-purple-accent-400">Draft Report - </span>
+                {props.data.dateCreated.substring(4, 16)}
+            </p>
+            <div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                <Link to="/genReport">
+                    {/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
+                    <div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
+                        <div className="text-left">
+                            {/* <button data-testid="btn-report" type="submit" onClick={viewGenReport}>
 								<p className="font-bold">VIEW REPORT</p>
 							</button> */}
-							<Button
-								text="View Report"
-								size="small"
-								handle={viewGenReport}
-								type="view"
-							/>
-						</div>
-					</div>
-				</Link>
-				{/* <div className="w-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center rounded-full px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
-				<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
-					{/* <button data-testid="btn-report" type="submit" onClick={deleteDraftHandler}>
+                            <Button
+                                text="View Report"
+                                size="small"
+                                handle={viewGenReport}
+                                type="view"
+                            />
+                        </div>
+                    </div>
+                </Link>
+                {/* <div className="w-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center rounded-full px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
+                <div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
+                    {/* <button data-testid="btn-report" type="submit" onClick={deleteDraftHandler}>
 						<p className="font-bold">DELETE REPORT</p>
 					</button> */}
-					<Button
-						text="Delete Report"
-						size="small"
-						handle={deleteDraftHandler}
-						type="delete"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+                    <Button
+                        text="Delete Report"
+                        size="small"
+                        handle={deleteDraftHandler}
+                        type="delete"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default DraftCard;
--- client/src/components/Explore/Explore.tsx
+++ client/src/components/Explore/Explore.tsx
@@ -7,161 +7,164 @@ import ExploreCard from "../ExploreCard/ExploreCard";
 import link from "../../resources/links.json";
 
 function Explore() {
-	const [report, changeReport] = useState<any[]>([]);
+    const [report, changeReport] = useState<any[]>([]);
 
-	// ######################### API FOR GETTING HISTORY #####################
+    // ######################### API FOR GETTING HISTORY #####################
 
-	let getAllReportsEndpoint =
-		process.env.NODE_ENV === "development"
-			? String(link.localhostLink)
-			: String(link.serverLink);
-	getAllReportsEndpoint += "getAllPublishedReports";
+    let getAllReportsEndpoint =
+        process.env.NODE_ENV === "development"
+            ? String(link.localhostLink)
+            : String(link.serverLink);
+    getAllReportsEndpoint += "getAllPublishedReports";
 
-	// using localhost
-	// const getAllMyDraftReportsEndpoint = "http://localhost:4000/dev/getAllMyReports";
+    // using localhost
+    // const getAllMyDraftReportsEndpoint = "http://localhost:4000/dev/getAllMyReports";
 
-	const getReports = async () => {
-		const apiData = {}; //empty json
+    const getReports = async () => {
+        const apiData = {}; //empty json
 
-		const requestOptions = {
-			method: "POST",
-			body: JSON.stringify(apiData)
-		};
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(apiData),
+            headers: {
+                "Authorization": `${localStorage.getItem("token")}`,
+            }
+        };
 
-		fetch(getAllReportsEndpoint, requestOptions)
-			.then(async (response) => {
-				const isJson = response.headers.get("content-type")?.includes("application/json");
+        fetch(getAllReportsEndpoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers.get("content-type")?.includes("application/json");
 
-				const data = isJson && (await response.json());
+                const data = isJson && (await response.json());
 
-				changeReport(await data);
+                changeReport(await data);
 
-				// check for error response
-				if (!response.ok) {
-					// error
-				}
-			})
-			.catch(() => {
-				// console.log("Error Getting History");
-			});
-	};
+                // check for error response
+                if (!response.ok) {
+                    // error
+                }
+            })
+            .catch(() => {
+                // console.log("Error Getting History");
+            });
+    };
 
-	getReports();
+    getReports();
 
-	// #######################################################################
+    // #######################################################################
 
-	//if(report.length === 0){
-	//	console.log("empty array");
-	//}
+    //if(report.length === 0){
+    //	console.log("empty array");
+    //}
 
-	return (
-		<div data-testid="report">
-			{/* Api response comes here */}
-			<div className=" mt-16 p-3 border-l border-gray-200">
-				<div className=" mt-4">
-					<h1 className="text-3xl hidden lg:block">Explore</h1>
+    return (
+        <div data-testid="report">
+            {/* Api response comes here */}
+            <div className=" mt-16 p-3 border-l border-gray-200">
+                <div className=" mt-4">
+                    <h1 className="text-3xl hidden lg:block">Explore</h1>
 
-					<div className="mt-4 flex flex-row flex-wrap justify-center">
-						<div
-							data-testid="reports"
-							className="mt-4 flex flex-row flex-wrap justify-center"
-						>
-							{report.map((data) => (
-								<div
-									className="m-4 w-auto h-auto bg-gray-400 hover:bg-gray-300 rounded-md flex flex-col p-2"
-									key={data.reportID}
-								>
-									<ExploreCard data={data} />
-								</div>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                    <div className="mt-4 flex flex-row flex-wrap justify-center">
+                        <div
+                            data-testid="reports"
+                            className="mt-4 flex flex-row flex-wrap justify-center"
+                        >
+                            {report.map((data) => (
+                                <div
+                                    className="m-4 w-auto h-auto bg-gray-400 hover:bg-gray-300 rounded-md flex flex-col p-2"
+                                    key={data.reportID}
+                                >
+                                    <ExploreCard data={data} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		// <div>
-		// 	{/* search */}
-		// 	<div className="flex justify-center p-2 border-l border-r border-b border-gray-200">
-		// 		<div className="w-3/4 mb-3">
-		// 			<input
-		// 				type="search"
-		// 				className="
-		//             nosubmit
-		//             w-full
-		//             px-3
-		//             py-1.5
-		//             text-lg
-		//             font-normal
-		//             text-gray-700
-		//             bg-clip-padding
-		//             border border-solid border-gray-300
-		//             rounded-lg
-		//             focus:text-gray-700 focus:bg-white focus:border-twitter-blue focus:outline-none
-		//             bg-gray-200
-		//           "
-		// 				value={enteredSearch}
-		// 				onChange={searchHandler}
-		// 				placeholder="search twitter summarizer..."
-		// 			/>
-		// 		</div>
-		// 	</div>
+        // <div>
+        // 	{/* search */}
+        // 	<div className="flex justify-center p-2 border-l border-r border-b border-gray-200">
+        // 		<div className="w-3/4 mb-3">
+        // 			<input
+        // 				type="search"
+        // 				className="
+        //             nosubmit
+        //             w-full
+        //             px-3
+        //             py-1.5
+        //             text-lg
+        //             font-normal
+        //             text-gray-700
+        //             bg-clip-padding
+        //             border border-solid border-gray-300
+        //             rounded-lg
+        //             focus:text-gray-700 focus:bg-white focus:border-twitter-blue focus:outline-none
+        //             bg-gray-200
+        //           "
+        // 				value={enteredSearch}
+        // 				onChange={searchHandler}
+        // 				placeholder="search twitter summarizer..."
+        // 			/>
+        // 		</div>
+        // 	</div>
 
-		// 	{/* Api response comes here */}
-		// 	<div className=" mt-4">
-		// 		{enteredSearch !== "" && <h1 className="text-2xl">Search results....</h1>}
+        // 	{/* Api response comes here */}
+        // 	<div className=" mt-4">
+        // 		{enteredSearch !== "" && <h1 className="text-2xl">Search results....</h1>}
 
-		// 		<div className="mt-4 flex flex-row flex-wrap justify-center">
-		// 			{tweeter.explore.map(
-		// 				(data) =>
-		// 					data.title.toLowerCase().match(enteredSearch.toLowerCase()) &&
-		// 					enteredSearch !== "" && (
-		// 						<div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
-		// 							<div className="">
-		// 								<button type="submit">
-		// 									<p className="font-bold">{data.title}</p>
-		// 								</button>
-		// 							</div>
-		// 							<div className="mt-2">
-		// 								<p className="italic text-xs">{data.name}</p>
-		// 							</div>
-		// 							<div className="">
-		// 								<p className="italic text-xs">{data.date}</p>
-		// 							</div>
-		// 						</div>
-		// 					)
-		// 			)}
-		// 		</div>
+        // 		<div className="mt-4 flex flex-row flex-wrap justify-center">
+        // 			{tweeter.explore.map(
+        // 				(data) =>
+        // 					data.title.toLowerCase().match(enteredSearch.toLowerCase()) &&
+        // 					enteredSearch !== "" && (
+        // 						<div className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2">
+        // 							<div className="">
+        // 								<button type="submit">
+        // 									<p className="font-bold">{data.title}</p>
+        // 								</button>
+        // 							</div>
+        // 							<div className="mt-2">
+        // 								<p className="italic text-xs">{data.name}</p>
+        // 							</div>
+        // 							<div className="">
+        // 								<p className="italic text-xs">{data.date}</p>
+        // 							</div>
+        // 						</div>
+        // 					)
+        // 			)}
+        // 		</div>
 
-		// 		{enteredSearch === "" && (
-		// 			<div className=" mt-4 p-3">
-		// 				<h1 className="text-2xl">Explore</h1>
+        // 		{enteredSearch === "" && (
+        // 			<div className=" mt-4 p-3">
+        // 				<h1 className="text-2xl">Explore</h1>
 
-		// 				<div className="mt-4 flex flex-row flex-wrap justify-center">
-		// 					{tweeter.explore.map((data) => (
-		// 						<div
-		// 							className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2"
-		// 							key={data.date}
-		// 						>
-		// 							<div className="">
-		// 								<button type="submit">
-		// 									<p className="font-bold">{data.title}</p>
-		// 								</button>
-		// 							</div>
-		// 							<div className="mt-2">
-		// 								<p className="italic text-xs">{data.name}</p>
-		// 							</div>
-		// 							<div className="">
-		// 								<p className="italic text-xs">{data.date}</p>
-		// 							</div>
-		// 						</div>
-		// 					))}
-		// 				</div>
-		// 			</div>
-		// 		)}
-		// 	</div>
-		// </div>
-	);
+        // 				<div className="mt-4 flex flex-row flex-wrap justify-center">
+        // 					{tweeter.explore.map((data) => (
+        // 						<div
+        // 							className="m-4 w-1/4 h-20 bg-gray-400 rounded-md flex flex-col p-2"
+        // 							key={data.date}
+        // 						>
+        // 							<div className="">
+        // 								<button type="submit">
+        // 									<p className="font-bold">{data.title}</p>
+        // 								</button>
+        // 							</div>
+        // 							<div className="mt-2">
+        // 								<p className="italic text-xs">{data.name}</p>
+        // 							</div>
+        // 							<div className="">
+        // 								<p className="italic text-xs">{data.date}</p>
+        // 							</div>
+        // 						</div>
+        // 					))}
+        // 				</div>
+        // 			</div>
+        // 		)}
+        // 	</div>
+        // </div>
+    );
 }
 
 export default Explore;
