9966fec0be7e2945cf231bd75bd0073289556ac9
Zenthon <u20536951@tuks.co.za>
2022-07-16 05:13:42 +0200

    ♻️ 🎨 💄 (UI): Enhanced the design of signup page

--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -1,15 +1,61 @@
 import "./Signup.css";
 import { BiErrorCircle } from "react-icons/bi";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Logo from "../Logo/Logo";
 import Button from "../Button/Button";
 import axios from "../../api/axios";
 
 function Signup(props: any) {
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const errRef = useRef();
+
     const [enteredUsername, changeEnteredUsername] = useState("");
+    const [validUsername, setValidUsername] = useState(false);
+    const [usernameFocus, setUsernameFocus] = useState(false);
+
     const [enteredEmail, changeEnteredEmail] = useState("");
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
     const [enteredPassword, changeEnteredPassword] = useState("");
+    const [validPassword, setValidPassword] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
+
     const [enteredConfirmPassword, changeEnteredConfirmPassword] = useState("");
+    const [validConfirmPassword, setValidConfirmPassword] = useState(false);
+    const [confirmPasswordFocus, setConfirmPasswordFocus] = useState(false);
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        usernameRef.current?.focus();
+    }, []);
+
+    useEffect(() => {
+        setValidUsername(/^[a-zA-Z0-9]{4,16}$/.test(enteredUsername));
+    }, [enteredUsername]);
+
+    useEffect(() => {
+        setValidEmail(
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+                enteredEmail
+            )
+        );
+    }, [enteredEmail]);
+
+    useEffect(() => {
+        setValidPassword(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                enteredPassword
+            )
+        );
+        setValidConfirmPassword(enteredPassword === enteredConfirmPassword);
+    }, [enteredPassword, enteredConfirmPassword]);
+
+    useEffect(() => {
+        setErrorMessage("");
+    }, [enteredUsername, enteredEmail, enteredPassword, enteredConfirmPassword]);
 
     const usernameHandler = (event: any) => {
         changeEnteredUsername(event.target.value);
@@ -29,79 +75,30 @@ function Signup(props: any) {
 
     const signup = async (userValidatedData: any) => {
         try {
-            await axios.post("signup",
-                JSON.stringify(userValidatedData), {
+            await axios.post("signup", JSON.stringify(userValidatedData), {
                 withCredentials: true
             });
             await props.readyToLogIN();
-        }
-        catch (error) {
-
-        }
+        } catch (error) { }
     };
 
     // #############################################################################
 
     // ########## CHECKING PASSWORD INPUT ################
     // creating variables for security checks
-    const [validName, setValidName] = useState(false);
-    const [validEmail, setValidEmail] = useState(false);
-    const [validLength, setValidLength] = useState(false);
-    const [hasNumber, setHasNumber] = useState(false);
-    const [upperCase, setUpperCase] = useState(false);
-    const [lowerCase, setLowerCase] = useState(false);
-    const [specialChar, setSpecialChar] = useState(false);
-    const [match, setMatch] = useState(false);
 
-    useEffect(() => {
-        setValidName(enteredUsername.length >= 2);
-        setValidEmail(
-            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-                enteredEmail
-            )
-        );
-        setValidLength(enteredPassword.length >= 8);
-        setUpperCase(enteredPassword.toLowerCase() !== enteredPassword);
-        setLowerCase(enteredPassword.toUpperCase() !== enteredPassword);
-        setHasNumber(/\d/.test(enteredPassword));
-        setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(enteredPassword));
-        setMatch(!!(enteredPassword && enteredPassword === enteredConfirmPassword));
-    }, [enteredUsername, enteredEmail, enteredPassword, enteredConfirmPassword]);
-
-    const [errorStatus, setError] = useState(false);
+    const [errorStatus, setError] = useState(true);
     const style = { fontSize: "2rem", color: "orange" };
 
     const submitHandler = (event: any) => {
         event.preventDefault();
-
-        // ######## CHECK DATABASE FOR EXISTING EMAIL ######## .... to do ....
-
-        // ###################################################
-
         const userDetails = {
             email: enteredEmail,
             username: enteredUsername,
             password: enteredPassword
         };
 
-        if (
-            validName === false ||
-            validLength === false ||
-            hasNumber === false ||
-            upperCase === false ||
-            lowerCase === false ||
-            specialChar === false ||
-            match === false
-        ) {
-            // console.log("It's false");
-
-            setError(true);
-        } else {
-            setError(false);
-            // console.log(userDetails);
-
-            signup(userDetails);
-        }
+        signup(userDetails);
     };
 
     const signin = (event: any) => {
@@ -122,83 +119,124 @@ function Signup(props: any) {
                 <h1 className="text-[#023E8A] text-center text-xl font-bold">Create new account</h1>
             </div>
             <br />
-            {/* unacceptable notification */}
-            {errorStatus && (
-                <div className="flex flex-row border-2 border-orange-700 rounded-md bg-orange-300 h-auto w-auto m-4 mb-5 p-2 items-center">
-                    <div className="p-2">
-                        <BiErrorCircle style={style} />
-                    </div>
-
-                    <div className="flex flex-col">
-                        {!validName && <p>Name should be more than 2 letters</p>}
-                        {!validEmail && <p>Please use a valid email address</p>}
-                        {!validLength && <p>password must be more than 8 characters</p>}
-                        {!specialChar && (
-                            <p>password must contain number, letters and special characters </p>
-                        )}
-                        {!upperCase && <p>password should have Uppercase</p>}
-                        {!lowerCase && <p>password should have Lowercase</p>}
-                        {!match && <p>password must match</p>}
-                    </div>
-                </div>
-            )}
-            {/*  */}
             <div>
                 <form
                     onSubmit={submitHandler}
                     action=""
-                    className="flex justify-center items-center flex-col"
+                    className="flex flex-col justify-center items-center "
                 >
                     <input
                         data-testid="name-input"
                         type="text"
+                        required
                         placeholder="Username"
                         autoComplete="off"
-                        className="w-56 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E]"
+                        className="w-60 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E]"
                         onChange={usernameHandler}
                         value={enteredUsername}
+                        aria-invalid={validUsername ? "true" : "false"}
+                        onFocus={() => setUsernameFocus(true)}
+                        onBlur={() => setUsernameFocus(false)}
                     />
+                    {usernameFocus && enteredUsername && !validUsername && (
+                        <div className="instructions flex flex-row border-2 rounded-md mt-4 bg-gray-100 h-auto w-60 p-2 justify-center items-center text-sm">
+                            <div className="flex flex-col">
+                                <p className="items-center justify-center">
+                                    Username should be 4-16 characters and alphanumeric
+                                </p>
+                            </div>
+                        </div>
+                    )}
                     <br />
-
                     <input
                         data-testid="email-input"
                         type="text"
                         placeholder="Email"
                         autoComplete="off"
-                        className="mb-3 w-56 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E] mb-6"
+                        className={
+                            validEmail
+                                ? "mb-3 w-60 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E] mb-6"
+                                : "mb-3 w-60 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E] mb-4"
+                        }
                         onChange={emailHandler}
                         value={enteredEmail}
+                        aria-invalid={validEmail ? "true" : "false"}
+                        onFocus={() => setEmailFocus(true)}
+                        onBlur={() => setEmailFocus(false)}
                     />
+                    {emailFocus && enteredEmail && !validEmail && (
+                        <div className="flex flex-row border-2 rounded-md bg-gray-100 h-10 w-60 justify-center p-2 mb-4 items-center text-sm">
+                            <div className="flex flex-col">
+                                <p>Should be a valid email</p>
+                            </div>
+                        </div>
+                    )}
 
-                    <div className="flex md:flex-row flex-col mb-3">
+                    <div className="flex flex-row flex-col mb-3">
                         <input
                             data-testid="password-input"
                             type="password"
                             placeholder="Password"
                             autoComplete="new-password"
-                            className=" w-56 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E]"
+                            className=" w-60 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E] "
                             onChange={passwordHandler}
                             value={enteredPassword}
+                            aria-invalid={validPassword ? "true" : "false"}
+                            onFocus={() => setPasswordFocus(true)}
+                            onBlur={() => setPasswordFocus(false)}
                         />
+                        {passwordFocus && enteredPassword && !validPassword && (
+                            <div className="flex flex-row border-2 rounded-md bg-gray-100 h-auto w-60 mini-tablet:w-auto p-2 mt-4 items-center text-sm">
+                                <div className="flex flex-col">
+                                    <p>
+                                        <strong>Password should be: </strong>
+                                    </p>
+                                    <ul>
+                                        <li>A Minimum of eight 10 characters</li>
+                                        <li>At least one uppercase letter</li>
+                                        <li>At least one lowercase letter</li>
+                                        <li>At least one number</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
                         &nbsp;
                         <input
                             data-testid="confirm-password-input"
                             type="password"
                             placeholder="Confirm Password"
                             autoComplete="off"
-                            className="mb-3 w-56 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E]"
+                            className="mb-3 w-60 h-10 border-gray-200 border rounded-md text-center text-md focus:outline-none focus:ring focus:border-[#023E8A] focus:text-[#03045E]"
                             onChange={passwordConfirmHandler}
                             value={enteredConfirmPassword}
+                            aria-invalid={validPassword ? "true" : "false"}
+                            onFocus={() => setConfirmPasswordFocus(true)}
+                            onBlur={() => setConfirmPasswordFocus(false)}
                         />
                     </div>
-                    <Button text="Sign Up" size="large" type="authentication" testid="btn-submit" />
+                    {((confirmPasswordFocus && enteredConfirmPassword && !validConfirmPassword) || (!confirmPasswordFocus && !validConfirmPassword)) && (
+                        <div className="flex flex-col flex-row border-2 rounded-md bg-gray-100 h-auto w-60 p-2 mb-4 items-center text-sm">
+                            <div className="flex flex-col">
+                                <p>
+                                    <strong>The passwords do not match</strong>
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                    <Button
+                        text="Sign Up"
+                        size="large"
+                        type="authentication"
+                        testid="btn-submit"
+                        disableId={(!validUsername || !validEmail || !validPassword || !validConfirmPassword) ? "true" : "false"}
+                    />
                     <br />
                     <p className="text-[#03045E] text-md text-center">
                         Already have an account?
                         <button
                             data-testid="btn-signin"
                             type="submit"
-                            className=" text-[#0096C7] hover:text-[#03045E]"
+                            className=" text-[#0096C7] "
                             onClick={signin}
                         >
                             &nbsp; Sign in
