d90e88df1afeca7798c25104f75a3f6b2e921dfa
Zenthon <u20536951@tuks.co.za>
2022-07-17 15:22:22 +0200

    💄 🔥 ✨ (UI): Changed the routing of the 3 pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,63 +1,23 @@
-import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import RequiredAuth from "./auth/RequiredAuth";
+import Layout from "./components/Layout";
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import Missing from "./components/Missing";
 
-// main Application component in which different page sub-components will be contained
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [signupPage, setSignupPage] = useState(false);
-    const [userApi, changeUserApi] = useState("");
-
-    useEffect(() => {
-        const storageUserLoggedInInformation = localStorage.getItem("key");
-        if (storageUserLoggedInInformation) {
-            setIsLoggedIn(true);
-        }
-    }, []);
-
-    // retrieving user login details
-    const loginHandler = (props: any) => {
-        console.log("In here")
-        if (props.login === "true") {
-            localStorage.setItem("key", props.apiKey);
-            localStorage.setItem("username", props.username);
-            localStorage.setItem("email", props.email);
-            localStorage.setItem("token", props.accessToken);
-            setIsLoggedIn(true);
-            setSignupPage(false);
-            changeUserApi(props.apiKey);
-        }
-    };
-
-    const signUpPage = () => {
-        setIsLoggedIn(false);
-        setSignupPage(true);
-    };
-
-    const logInPage = () => {
-        localStorage.clear();
-        setIsLoggedIn(false);
-        setSignupPage(false);
-    };
-
-    const readyToLog = () => {
-        localStorage.setItem("newUser", "true");
-        setSignupPage(false);
-    };
-
     return (
-        <div className="">
-            {!localStorage.getItem("key") && !signupPage && (
-                <Login userLoginDetails={loginHandler} takeToSignupPage={signUpPage} />
-            )}
-
-            {/* Signup */}
-            {signupPage && <Signup takeToSigninPage={logInPage} readyToLogIN={readyToLog} />}
-
-            {/* Entry here based on Signup and Login decision  */}
-            {isLoggedIn && <Landing userAPI={userApi} takeToSigninPage={logInPage} />}
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />} >
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+                <Route element={<RequiredAuth />} >
+                    <Route path="/" element={<Landing />} />
+                </Route>
+                <Route path="*" element={<Missing />} />
+            </Route>
+        </Routes>
     );
 }
 
