0d3543af4451b764ce1c0ea0ee32da033b191c1f
Zenthon <u20536951@tuks.co.za>
2022-07-21 14:18:16 +0200

    ✨ (UI): Added Persistent Login

--- client/src/components/PersistLogin.tsx
+++ client/src/components/PersistLogin.tsx
@@ -5,13 +5,35 @@ import useAuth from "../hooks/useAuth";
 
 function PersistLogin() {
     const [isLoading, setIsLoading] = useState(true);
-    const refreh = useRefreshToken();
-    const { auth } = useAuth();
+    const refresh = useRefreshToken();
+    const { auth, setAuth } = useAuth();
 
     useEffect(() => {
+        const verifyRefreshToken = async () => {
+            try {
+                const newToken = await refresh();
+                setAuth((prev: any) => { return { ...prev, accessToken: newToken } });
+            } catch (error) {
+                console.error(error);
 
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        console.log("In use effect");
+        !auth.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    }, []);
 
-    })
+    useEffect(() => {
+        console.log(`isLoading: ${isLoading}`);
+        console.log(`auth: ${JSON.stringify(auth?.accessToken)}`);
+    }, [isLoading]);
+
+    return (
+        <>
+            {isLoading ? <div>Loading...</div> : <Outlet />}
+        </>
+    );
 }
 
 export default PersistLogin;
