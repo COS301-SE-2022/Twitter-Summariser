3fadfd3d7a66cbf71b94d8dfe253f4b2a443a2b4
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-21 20:20:26 +0200

    :zap::sparkles: added code structure for the scheduler function

---
+++ src/functions/schedulers/handler.ts
@@ -0,0 +1,26 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { middyfy } from "@libs/lambda";
+import { header, statusCodes } from "@functions/resources/APIresponse";
+import ServicesLayer from "../../services";
+
+export const reportSchduler = middyfy(
+	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+		try {
+			const params = JSON.parse(event.body);
+
+			const resultSet = await ServicesLayer.resultSetServices.getResultSets(params.apiKey);
+
+			return {
+				statusCode: statusCodes.Successful,
+				headers: header,
+				body: JSON.stringify(resultSet)
+			};
+		} catch (e) {
+			return {
+				statusCode: statusCodes.internalError,
+				headers: header,
+				body: JSON.stringify(e)
+			};
+		}
+	}
+);
