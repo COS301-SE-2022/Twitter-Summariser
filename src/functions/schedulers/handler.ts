c7b8d3747d4a11cb8944a1c44dfb26177feb07f3
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-21 21:00:13 +0200

    :zap::sparkles: added eventbridge for scheduling event.

--- src/functions/schedulers/handler.ts
+++ src/functions/schedulers/handler.ts
@@ -2,13 +2,30 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { middyfy } from "@libs/lambda";
 import { header, statusCodes } from "@functions/resources/APIresponse";
 import ServicesLayer from "../../services";
+import *  as AWS from 'aws-sdk';
+
+// Importing Lambda Attributes
+const eventBridge = new AWS.EventBridge();
+const lambda = new AWS.Lambda();
 
 export const reportSchduler = middyfy(
 	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 		try {
 			const params = JSON.parse(event.body);
 
-			const resultSet = await ServicesLayer.resultSetServices.getResultSets(params.apiKey);
+			// Making eventBridge to trigger event at certain time
+			const ruleName = "makingReport";
+			const ruleParams = {
+				Name: ruleName,
+				ScheduleExpression: "",
+			};
+
+			const rule = await eventBridge.putRule(ruleParams).promise;
+
+			// Giving correct permissions
+			const permissionParams = {
+				Action: 'lambda'
+			}
 
 			return {
 				statusCode: statusCodes.Successful,
