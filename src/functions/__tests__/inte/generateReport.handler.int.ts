366b3a26d191a484d230ac182e167073b907471b
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-27 18:13:24 +0200

    :truck::zap::sparkles: readded tests hopefully they don't fail

---
+++ src/functions/__tests__/inte/generateReport.handler.int.ts
@@ -0,0 +1,18 @@
+import * as request from 'superagent';
+
+const apiAcesspoint = {
+    'development': 'https://xprnnqlwwi.execute-api.us-east-1.amazonaws.com/dev/'
+}
+
+describe('Given an authorised request WHEN the post generateReport endpoint is called', () => {
+    test('THEN it should respond with a 200', async () => {
+        const { statusCode } = await request.post(apiAcesspoint.development+"generateReport");
+        expect(statusCode).toBe(200);
+        
+    });
+
+    test('THEN it should respond with a healthy body', async () => {
+        const { body } = await request.post(apiAcesspoint.development+"generateReport");
+        expect(body).toBeTruthy;
+    });
+})
---
+++ src/functions/__tests__/inte/lambda.dynamoDB.int.ts
@@ -0,0 +1,76 @@
+// import * as AWS from "aws-sdk";
+import serviceLayer from "../../../services/index";
+
+// const lambda = new AWS.Lambda();
+
+/* describe(" ", async () => {
+	// Creation of table
+	await dynamoDB
+		.put({
+			TableName: "Users",
+			Item: {
+				HashKey: "userID"
+			}
+		})
+		.promise();
+
+	// Invoking Lambda function
+	await lambda
+		.invoke({
+			FunctionName: "createAdminFile",
+			Payload: JSON.stringify({
+				userID: "userId",
+				filename: "sample.txt",
+				content: "ok"
+			})
+		})
+		.promise();
+
+	// Creating S3 Bucket Object
+	const s3Object = await s3
+		.getObject({
+			Bucket: "admin-files",
+			Key: "sample.txt"
+		})
+		.promise();
+
+	// Deleting table
+	await dynamoDB
+		.delete({
+			TableName: "Users",
+			Key: { HashKey: "userID" }
+		})
+		.promise();
+
+	// Deleting s3 Object
+	await s3
+		.deleteObject({
+			Bucket: "admin-files",
+			Key: "sample.txt"
+		})
+		.promise();
+}); */
+
+describe("Checks the correct writing of a creator to a database.", async () => {
+	// Envoking Lambda function
+	/* const creator = await lambda
+		.invoke({
+			FunctionName: "addCreator",
+			Payload: JSON.stringify({
+				email: "test@gmail.com",
+				username: "Integration Test",
+				password: "IntegrationTest@",
+				dateOfBirth: "2001/01/01"
+			})
+		})
+		.promise(); */
+
+	// reading from database
+	const databaseRead = await serviceLayer.creatorService.getCreator("test@gmail.com");
+
+	// Checking return status
+	expect(databaseRead!==undefined);
+
+	// Checks if write was valid
+	// expect(creator.$response.data === databaseRead);
+});
