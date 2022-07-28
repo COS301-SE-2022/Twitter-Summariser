760e08256b417734c6a1b386014550367e354f3b
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 11:05:45 +0200

    :fire: swapped from fetch to axios

--- src/functions/__tests__/inte/back-end.int.ts
+++ src/functions/__tests__/inte/back-end.int.ts
@@ -1,6 +1,8 @@
-const {fetch} = require('node-fetch');
+
 
 // Tests with post requests Testing integration of API --> Lambda --> DynamodbDB
+
+import axios from "axios";
 
 
 const apiAcesspoint = {
@@ -9,14 +11,19 @@ const apiAcesspoint = {
 
 describe("Given a valid API request to add a user", async () => {
 	// Making api call
-	const response = await fetch(apiAcesspoint + "signup", {
-		method: "POST",
-		body: JSON.stringify({ email: "test@gmail.com", username: "test", password: "test" }),
-		headers: { "content-Type": "application/json" }
-	});
+	const response = await axios.post(
+		apiAcesspoint.development+'signup',
+		{ username: 'test', email: 'test@gmail.com', password: 'test' },
+		{
+		  headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		  },
+		},
+	  );
 
 	test("Make sure api recevies request and returns success status", async () => {
-		expect(response.ok);
+		expect(response).toBeDefined;
 	});
 
 	test("Make sure api returns success status", async () => {
@@ -25,14 +32,14 @@ describe("Given a valid API request to add a user", async () => {
 
 	test("Make sure that backend has correctly processed data and return avlid data", async () => {
 		// Expect The correct output
-		expect(response.json["apiKey"]).toBeDefined;
-		expect(response.json["email"]).toBeDefined;
-		expect(response.json["username"]).toBeDefined;
+		expect(response.data["apiKey"]).toBeDefined;
+		expect(response.data["email"]).toBeDefined;
+		expect(response.data["username"]).toBeDefined;
 	});
 
 	test("Make sure that backend has correctly processed data and return correct data", async () => {
 		//Expect the Correct details to be returned
-		expect(response.json["username"]).toEqual("test");
+		expect(response.data["username"]).toEqual("test");
 	});
     
 });
