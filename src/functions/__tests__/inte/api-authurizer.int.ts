62611803c804655cb3a30cfd8e2f376663692a0c
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 12:55:39 +0200

    :bug:

--- src/functions/__tests__/inte/api-authurizer.int.ts
+++ src/functions/__tests__/inte/api-authurizer.int.ts
@@ -8,19 +8,9 @@ const apiAcesspoint = {
 describe("Given an authorised request WHEN the post editBlock endpoint is called", () => {
 	
 	it('it should make a valid ', async () => {
-		//const response = await request.post(apiAcesspoint.development + "editblock");
-
-		try{
-			const response = await request.post(apiAcesspoint.development + "editblock");
-
-			test("THEN it should respond with a 200", () => {
-				expect(response.statusCode).toBe(401);
-			});
-		}catch (e){
-			test("THEN it should respond with a 200", () => {
-				expect(e).toBe('Forbidden');
-			});
-		}
+		await request.post(apiAcesspoint.development + "editblock").then((statusCode) =>{
+			expect(statusCode).toBe(401);
+		});
 
 		/*test("THEN it should respond with a healthy body", async () => {
 			expect(response.body).toBe(' Unauthorized');
