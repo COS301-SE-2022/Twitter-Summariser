d6860d56aed90e2cded562da4b762d30c1150f04
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 08:57:06 +0200

    :bug::zap: readded incorrectly removed code

---
+++ src/functions/__tests__/inte/api-lambda.int.ts
@@ -0,0 +1,31 @@
+import * as request from 'superagent';
+
+const apiAcesspoint = {
+    'development': 'https://3vdykbiva3.execute-api.us-east-1.amazonaws.com/dev/'
+}
+
+describe('Given an authorised request WHEN the post editBlock endpoint is called', () => {
+    test('THEN it should respond with a 200', async () => {
+        const { statusCode } = await request.post(apiAcesspoint.development+"editBlock");
+        expect(statusCode).toBe(200);
+        
+    });
+
+    test('THEN it should respond with a healthy body', async () => {
+        const { body } = await request.post(apiAcesspoint.development+"editBlock");
+        expect(body).toBeTruthy;
+    });
+})
+
+describe('Given an authorised request WHEN the post generateReport endpoint is called', () => {
+    test('THEN it should respond with a 200', async () => {
+        const { statusCode } = await request.post(apiAcesspoint.development+"generateReport");
+        expect(statusCode).toBe(200);
+        
+    });
+
+    test('THEN it should respond with a healthy body', async () => {
+        const { body } = await request.post(apiAcesspoint.development+"generateReport");
+        expect(body).toBeTruthy;
+    });
+})
