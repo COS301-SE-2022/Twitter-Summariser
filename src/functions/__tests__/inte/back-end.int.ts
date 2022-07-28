cc8dcdd67c39e4606723b6e8c2e9b759ba484c5a
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 14:03:37 +0200

    :white_checked_mark:

--- src/functions/__tests__/inte/back-end.int.ts
+++ src/functions/__tests__/inte/back-end.int.ts
@@ -15,29 +15,61 @@ describe("Given a valid API request to add a user", () => {
 				"signup",
 				JSON.stringify({ username: "test", email: "test@gmail.com", password: "test" })
 			);
+		} catch (error) {
+			response = "error";
+		}
 
-		}catch(error){
-			response='error';
+		test("Make sure api recevies request and returns success status", async () => {
+			expect(response).toBeDefined;
+		});
+	});
+
+	it("it should make a valid ", async () => {
+		try {
+			response = await axios.post(
+				"signup",
+				JSON.stringify({ username: "test", email: "test@gmail.com", password: "test" })
+			);
+		} catch (error) {
+			response = "error";
 		}
 
 		test("Make sure api returns success status", async () => {
 			expect(response.status).toBe(200);
 		});
 	});
-		
-		test("Make sure api recevies request and returns success status", async () => {
-				expect(response).toBeDefined;
+
+	it("it should make a valid ", async () => {
+		try {
+			response = await axios.post(
+				"signup",
+				JSON.stringify({ username: "test", email: "test@gmail.com", password: "test" })
+			);
+		} catch (error) {
+			response = "error";
+		}
+
+		test("Make sure that backend has correctly processed data and return avlid data", async () => {
+			// Expect The correct output
+			expect(response.data["apiKey"]).toBeDefined;
+			expect(response.data["email"]).toBeDefined;
+			expect(response.data["username"]).toBeDefined;
 		});
+	});
 
-			test("Make sure that backend has correctly processed data and return avlid data", async () => {
-				// Expect The correct output
-				expect(response.data["apiKey"]).toBeDefined;
-				expect(response.data["email"]).toBeDefined;
-				expect(response.data["username"]).toBeDefined;
-			});
+	it("it should make a valid ", async () => {
+		try {
+			response = await axios.post(
+				"signup",
+				JSON.stringify({ username: "test", email: "test@gmail.com", password: "test" })
+			);
+		} catch (error) {
+			response = "error";
+		}
 
-			test("Make sure that backend has correctly processed data and return correct data", async () => {
-				//Expect the Correct details to be returned
-				expect(response.data["username"]).toEqual("test");
-			});
+		test("Make sure that backend has correctly processed data and return correct data", async () => {
+			//Expect the Correct details to be returned
+			expect(response.data["username"]).toEqual("test");
+		});
+	});
 });
