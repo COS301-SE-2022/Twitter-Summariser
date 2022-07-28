1959baf6e7a0bf0aeee0e54b9232f2efd9e28dde
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 12:20:52 +0200

    :bug: try fix for test

--- src/functions/__tests__/inte/api-authurizer.int.ts
+++ src/functions/__tests__/inte/api-authurizer.int.ts
@@ -6,25 +6,30 @@ const apiAcesspoint = {
 
 // Tests using super agent
 describe("Given an authorised request WHEN the post editBlock endpoint is called", () => {
-	test("THEN it should respond with a 200", async () => {
-		const { statusCode } = await request.post(apiAcesspoint.development + "editBlock");
-		expect(statusCode).toBe(401);
-	});
+	
+	it('it should make a valid ', async () => {
+		const response = await request.post(apiAcesspoint.development + "editblock");
 
-	test("THEN it should respond with a healthy body", async () => {
-		const { body } = await request.post(apiAcesspoint.development + "editBlock");
-		expect(body).toBeTruthy;
+		test("THEN it should respond with a 200", async () => {
+			expect(response.statusCode).toBe(401);
+		});
+
+		test("THEN it should respond with a healthy body", async () => {
+			expect(response.body).toBe(' Unauthorized');
+		});
 	});
 });
 
 describe("Given an authorised request WHEN the post generateReport endpoint is called", () => {
-	test("THEN it should respond with a 200", async () => {
-		const { statusCode } = await request.post(apiAcesspoint.development + "generateReport");
-		expect(statusCode).toBe(401);
-	});
+	it('it should make a valid ', async () => {
+		const response = await request.post(apiAcesspoint.development + "editblock");
 
-	test("THEN it should respond with a healthy body", async () => {
-		const { body } = await request.post(apiAcesspoint.development + "generateReport");
-		expect(body).toBeTruthy;
+		test("THEN it should respond with a 200", async () => {
+			expect(response.statusCode).toBe(401);
+		});
+
+		test("THEN it should respond with a healthy body", async () => {
+			expect(response.body).toBe(' Unauthorized');
+		});
 	});
 });
--- src/functions/__tests__/inte/api-lambda.int.ts
+++ src/functions/__tests__/inte/api-lambda.int.ts
@@ -6,25 +6,30 @@ const apiAcesspoint = {
 
 // Tests using super agent
 describe("Given an authorised request WHEN the post editBlock endpoint is called", () => {
-	test("THEN it should respond with a 200", async () => {
-		const { statusCode } = await request.post(apiAcesspoint.development + "signin");
-		expect(statusCode).toBe(401);
-	});
+	
+	it('it should make a valid ', async () => {
+		const response = await request.post(apiAcesspoint.development + "searchTweets");
 
-	test("THEN it should respond with a healthy body", async () => {
-		const { body } = await request.post(apiAcesspoint.development + "signin");
-		expect(body).toBe(' Unauthorized');
+		test("THEN it should respond with a 200", async () => {
+			expect(response.statusCode).toBe(200);
+		});
+
+		test("THEN it should respond with a healthy body", async () => {
+			expect(response.body).toBe(' Unauthorized');
+		});
 	});
 });
 
 describe("Given an authorised request WHEN the post generateReport endpoint is called", () => {
-	test("THEN it should respond with a 200", async () => {
-		const { statusCode } = await request.post(apiAcesspoint.development + "signup");
-		expect(statusCode).toBe(401);
-	});
+	it('it should make a valid ', async () => {
+		const response = await request.post(apiAcesspoint.development + "getReport");
 
-	test("THEN it should respond with a healthy body", async () => {
-		const { body } = await request.post(apiAcesspoint.development + "signup");
-		expect(body).toBeTruthy;
+		test("THEN it should respond with a 200", async () => {
+			expect(response.statusCode).toBe(200);
+		});
+
+		test("THEN it should respond with a healthy body", async () => {
+			expect(response.body).toBe(' Unauthorized');
+		});
 	});
 });
