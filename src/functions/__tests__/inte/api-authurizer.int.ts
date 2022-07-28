8c5bd2d9132ab92518f55faa31c703a8008696d0
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 12:47:25 +0200

    :bug:

--- src/functions/__tests__/inte/api-authurizer.int.ts
+++ src/functions/__tests__/inte/api-authurizer.int.ts
@@ -8,11 +8,19 @@ const apiAcesspoint = {
 describe("Given an authorised request WHEN the post editBlock endpoint is called", () => {
 	
 	it('it should make a valid ', async () => {
-		const response = await request.post(apiAcesspoint.development + "editblock");
+		//const response = await request.post(apiAcesspoint.development + "editblock");
 
-		test("THEN it should respond with a 200", () => {
-			expect(response.forbidden);
-		});
+		try{
+			const response = await request.post(apiAcesspoint.development + "editblock");
+
+			test("THEN it should respond with a 200", () => {
+				expect(response.statusCode).toBe(401);
+			});
+		}catch (e){
+			test("THEN it should respond with a 200", () => {
+				expect(e).toBe('Forbidden');
+			});
+		}
 
 		/*test("THEN it should respond with a healthy body", async () => {
 			expect(response.body).toBe(' Unauthorized');
@@ -22,11 +30,17 @@ describe("Given an authorised request WHEN the post editBlock endpoint is called
 
 describe("Given an authorised request WHEN the post generateReport endpoint is called", () => {
 	it('it should make a valid ', async () => {
-		const response = await request.post(apiAcesspoint.development + "editblock");
+		try{
+			const response = await request.post(apiAcesspoint.development + "generateReport");
 
-		test("THEN it should respond with a 200", () => {
-			expect(response.statusCode).toBe(401);
-		});
+			test("THEN it should respond with a 200", () => {
+				expect(response.statusCode).toBe(401);
+			});
+		}catch (e){
+			test("THEN it should respond with a 200", () => {
+				expect(e).toBe(401);
+			});
+		}
 
 		/*test("THEN it should respond with a healthy body", async () => {
 			expect(response.body).toBe(' Unauthorized');
