3606c22d086c9be7532791c54ce8bbbb2b39b84f
malcolmdzimati <malcolmdzimati@gmail.com>
2022-07-28 09:27:11 +0200

    :test_tube::zap: added int test

--- src/functions/__tests__/inte/api-lambda.int.ts
+++ src/functions/__tests__/inte/api-lambda.int.ts
@@ -1,9 +1,11 @@
 import * as request from 'superagent';
+import fetch from 'node-fetch'
 
 const apiAcesspoint = {
     'development': 'https://pgxz0lthzj.execute-api.us-east-1.amazonaws.com/dev/'
 }
 
+// Tests using super agent
 describe('Given an authorised request WHEN the post editBlock endpoint is called', () => {
     test('THEN it should respond with a 200', async () => {
         const { statusCode } = await request.post(apiAcesspoint.development+"editBlock");
@@ -28,4 +30,18 @@ describe('Given an authorised request WHEN the post generateReport endpoint is c
         const { body } = await request.post(apiAcesspoint.development+"generateReport");
         expect(body).toBeTruthy;
     });
+})
+
+// Tests with post requests
+
+describe('Given ', () => {
+    test('',async () => {
+        const response = await fetch(apiAcesspoint+'signup', {
+            method: 'POST',
+            body: JSON.stringify({'email': 'test@gmail.com', 'username': 'test', 'password': 'test'}),
+            headers:{'content-Type': 'application/json'}
+        })
+
+        expect(response.ok);
+    })
 })
