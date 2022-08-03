06d559ede164d383db59e9162c6d28eb2fd9d971
Zenthon <u20536951@tuks.co.za>
2022-08-03 16:44:37 +0200

    🧱 ✨ (CICD): Added infra for ai module

---
+++ text-summarisation/serverless.ts
@@ -0,0 +1,31 @@
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+    service: "text-summarisation",
+    frameworkVersion: "3",
+    provider: {
+        name: "aws",
+        runtime: "python3.8"
+
+    },
+    plugins: [
+        "serverless-offline"
+    ],
+
+    functions: {
+        hello: {
+            handler: "handler.hello",
+            events: [
+                {
+                    http: {
+                        method: "get",
+                        path: "hello/"
+                    }
+                }
+            ]
+        }
+    }
+
+}
+
+module.exports = serverlessConfiguration;
