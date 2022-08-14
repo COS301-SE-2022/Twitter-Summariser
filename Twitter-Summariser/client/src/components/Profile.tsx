dbd60ec0cd1f2c9a58eac147bd2cb34d4fbb1c79
GO-Shoderu <go.shoderu@gmail.com>
2022-08-14 11:47:37 +0200

    💄 (UI): Added retrieved user information [Username and Email]

--- Twitter-Summariser/client/src/components/Profile.tsx
+++ Twitter-Summariser/client/src/components/Profile.tsx
@@ -1,6 +1,17 @@
 import "./styles/Profile.css";
+// import { AxiosError } from "axios";
+
+import useAuth from "../hooks/useAuth";
 
 function Profile() {
+	const { auth } = useAuth();
+
+	const userInformation = {
+		apiKey: auth.apiKey,
+		author: auth.username,
+		email: auth.email
+	};
+
 	return (
 		<div data-testid="profile">
 			<div className="flex flex-col items-center mt-3 p-3">
@@ -15,7 +26,10 @@ function Profile() {
 					/>
 				</div>
 				{/* div for name, emails, number of reports published, share icon etc */}
-				<div className="h-40 w-2/3 text-center">other details comes here</div>
+				<div className="h-40 w-2/3 text-center flex flex-col">
+					<div className=" text-3xl font-bold">{userInformation.author}</div>
+					<div className=" italic">{userInformation.email}</div>
+				</div>
 			</div>
 		</div>
 	);
