d77c55c5c330a1e784c77b4d5f2e9703fe8853fd
GO-Shoderu <go.shoderu@gmail.com>
2022-08-18 11:22:40 +0200

    💄(UI): Added a trigger button to cancel the add custom tweet modal

--- app/client/src/components/Modal.tsx
+++ app/client/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Modal({ setModalOn, setChoice }: any) {
 	const handleOKClick = () => {
@@ -6,16 +6,24 @@ function Modal({ setModalOn, setChoice }: any) {
 		setModalOn(false);
 		console.log("setModal false");
 	};
+
 	const handleCancelClick = () => {
 		setChoice(false);
 		setModalOn(false);
 	};
+
+	const style = { fontSize: "1.3rem" };
 
 	return (
 		<div className="flex justify-center items-center z-54 px-5">
 			<div className="bg-zinc-200 opacity-90 fixed inset-0" />
 			<div className="flex justify-center items-center inset-0 fixed">
 				<div className="bg-white border-4 opacity-100 border-sky-500 rounded-xl ">
+					<div className="relative">
+						<div className="absolute top-0 right-0 p-3 cursor-pointer text-red-600">
+							<AiOutlineClose style={style} onClick={handleCancelClick} />
+						</div>
+					</div>
 					<div className="flex-col justify-center   py-12 px-24 ">
 						<div className="flex  text-lg  text-zinc-600   mb-10">Are you sure ?</div>
 						<div className="flex">
