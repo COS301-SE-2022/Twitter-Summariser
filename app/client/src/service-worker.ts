4380bdd6933baf30325848335420cc20ffad1310
Tlholo Koma <tlholokoma@gmail.com>
2022-08-20 00:48:43 +0200

    :heavy_plus_sign: :construction: :hammer: :card_file_box: Create database locally

--- app/client/src/service-worker.ts
+++ app/client/src/service-worker.ts
@@ -12,8 +12,15 @@ import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
+import { openDB } from "idb"; 
 
 declare const self: ServiceWorkerGlobalScope;
+
+const dbCache = openDB('Cache-Requests', 1, {
+	upgrade(db) {
+		db.createObjectStore('post-cache');
+	}
+});
 
 self.addEventListener("fetch", async (event)=> {
 	
