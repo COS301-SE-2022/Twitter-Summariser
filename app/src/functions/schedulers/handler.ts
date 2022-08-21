3465e50010e29449c75cd840c06d3d00a1f808f0
malcolmdzimati <malcolmdzimati@gmail.com>
2022-08-21 15:02:39 +0200

    :zap::sparkles: added secomd handler

--- app/src/functions/schedulers/handler.ts
+++ app/src/functions/schedulers/handler.ts
@@ -1,8 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { middyfy } from "@libs/lambda";
+import { header, statusCodes } from "@functions/resources/APIresponse";
+import ServicesLayer from "../../services";
 
 // Generation of reports
-export const generateReport = middyfy(
+export const reportScheduler = middyfy(
+	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+		try {
+			const params = JSON.parse(event.body);
+			
+
+			return {
+				statusCode: statusCodes.Successful,
+				headers: header,
+				body: JSON.stringify('')
+			};
+		} catch (e) {
+			return {
+				statusCode: statusCodes.internalError,
+				headers: header,
+				body: JSON.stringify(e)
+			};
+		}
+	}
+);
+
+export const genScheduledReport = middyfy(
 	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 		try {
 			const params = JSON.parse(event.body);
