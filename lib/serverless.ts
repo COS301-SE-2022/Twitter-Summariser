7b41cc7c00375444356081c2c191110ff53c5179
Zenthon <u20536951@tuks.co.za>
2022-08-26 15:33:26 +0200

    ♻️  🔥 ((cicd): Changed configFile for deployment

--- lib/serverless.ts
+++ lib/serverless.ts
@@ -5,52 +5,24 @@ const serverlessConfiguration: AWS = {
     frameworkVersion: "3",
     provider: {
         name: "aws",
-        runtime: "python3.8"
+        runtime: "python3.8",
+        region: "us-east-1",
+        timeout: 30,
 
-    },
-    plugins: [
-        "serverless-offline",
-        "serverless-python-requirements"
-    ],
-
-    package: {
-        exclude: [
-            "**/*"
-        ],
-        include: [
-            "*.py",
-            "pandas",
-            "numpy"
-        ]
     },
 
     functions: {
-        hello: {
-            handler: "handler.hello",
+        summarise: {
+            image: "534808114586.dkr.ecr.us-east-1.amazonaws.com/text-summarisation@sha256:a1c70cf75819244b22e0fb285a3454c389421a5f9ecccdc1b22b183630f1cf09",
             events: [
                 {
                     http: {
-                        method: "get",
-                        path: "hello/"
+                        method: "post",
+                        path: "summarise"
                     }
                 }
             ]
         }
-    },
-
-    custom: {
-        "serverless-offline": {
-            httpPort: 5000,
-            websocketPort: 5001,
-            lambdaPort: 5002
-        },
-
-        "pythonRequirements": {
-            dockerizePip: false,
-            pythonBin: "python3",
-            zip: true
-        }
-
     }
 };
 
