2e0d260b1d47afe087d1aff4de242b8b986d399d
jonahgasura <u20592061@tuks.co.za>
2022-08-30 23:15:11 +0200

    ✨ (UI): Updated draft report link in explore card to navigate to URL with specific report ID and removed use of local storage

--- app/client/src/components/ExploreCard.tsx
+++ app/client/src/components/ExploreCard.tsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom";
 import Button from "./Button";
 
 function ExploreCard(props: any) {
+
+	const repID = props.data.reportID;
+	const newReportLink = `/report/${repID}`;
+
+
 	const viewReport = () => {
-		if (localStorage.getItem("reportId")) {
-			localStorage.removeItem("reportId");
-			localStorage.setItem("reportId", props.data.reportID);
-		} else {
-			localStorage.setItem("reportId", props.data.reportID);
-		}
+		// if (localStorage.getItem("reportId")) {
+		// 	localStorage.removeItem("reportId");
+		// 	localStorage.setItem("reportId", props.data.reportID);
+		// } else {
+		// 	localStorage.setItem("reportId", props.data.reportID);
+		// }
 	};
 
 	return (
@@ -79,7 +84,7 @@ function ExploreCard(props: any) {
 				</span>
 			</p>
 			<div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-				<Link to="/report">
+				<Link to={newReportLink}>
 					{/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
 					<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
 						<div className="text-left">
