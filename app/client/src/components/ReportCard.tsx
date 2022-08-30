5be2eb2e1ab9e6baf88f19ae3eea76df439cd279
jonahgasura <u20592061@tuks.co.za>
2022-08-30 17:44:07 +0200

    ✨ 🎨 (UI): Updated use of newly named Report component in reportCard component

--- app/client/src/components/ReportCard.tsx
+++ app/client/src/components/ReportCard.tsx
@@ -98,7 +98,7 @@ function ReportCard(props: any) {
 				</span>
 			</p>
 			<div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-				<Link to="/getPublishedReport">
+				<Link to="/report">
 					{/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
 					<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
 						<div className="text-left">
