6ca43da8706d4e17fd6262ce68f9d09996aa1192
Zenthon <u20536951@tuks.co.za>
2022-09-02 22:50:25 +0200

    ♻️ 🔥 (UI): Removed auth decoding and verification

--- app/client/src/auth/RequiredAuth.tsx
+++ app/client/src/auth/RequiredAuth.tsx
@@ -1,17 +1,9 @@
 import { Navigate, useLocation, Outlet } from "react-router-dom";
-import jwt from "jwt-decode";
 import useAuth from "../hooks/useAuth";
 
 function RequiredAuth() {
 	const { auth } = useAuth();
 	const location = useLocation();
-
-	if (auth?.accessToken) {
-		const decodedToken = jwt(auth.accessToken);
-		auth.username = (decodedToken as any).username;
-		auth.apiKey = (decodedToken as any).apiKey;
-		auth.email = (decodedToken as any).email;
-	}
 
 	return auth?.accessToken ? (
 		<Outlet />
