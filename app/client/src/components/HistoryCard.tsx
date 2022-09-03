85ef40bbe5f50bd7546a16f8f7b1c38ca68990f9
Zenthon <u20536951@tuks.co.za>
2022-09-03 20:30:59 +0200

    🔥 💄 (UI): Enhanced the Search History Cards

--- app/client/src/components/HistoryCard.tsx
+++ app/client/src/components/HistoryCard.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import Button from "./Button";
 
 function HistoryCard(props: any) {
 	const axiosPrivate = useAxiosPrivate();
@@ -44,63 +43,15 @@ function HistoryCard(props: any) {
 	}
 
 	return (
-		// <div className="">
-		//     <div className="m-4 w-auto h-20 p-2 flex flex-row justify-between items-center">
-		//         <div className="">
-		//             <div className="">
-		//                 <Link to="/viewHistory">
-		//                     <button type="submit" onClick={setResultSet}>
-		//                         <p className="font-bold">{props.data.searchPhrase}</p>
-		//                     </button>
-		//                 </Link>
-		//             </div>
-		//             <div className="mt-2">
-		//                 <p className="italic text-xs">Sorted: {props.data.sortOption}</p>
-		//             </div>
-		//             <div className="mt-2">
-		//                 <p className="italic text-xs">Filtered: {props.data.filterOption}</p>
-		//             </div>
-		//             <div className="">
-		//                 <p className="italic text-xs">{props.data.date}</p>
-		//             </div>
-		//         </div>
-
-		//         <button type="button" onClick={deleteHandler}>
-		//             <div className=" ml-2" data-bs-toggle="tooltip" title="Delete History">
-		//                 <MdDeleteOutline style={iconStyle3} />
-		//             </div>
-		//         </button>
-		//     </div>
-		// </div>
-		<div className="p-8 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
-			{/* <div className="flex items-center justify-center">
-				<p aria-label="Author" title="Author" className="mr-3 ">
-					<img
-						src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-						alt="avatar"
-						className="object-cover w-10 h-10 rounded-full shadow-sm"
-					/>
-				</p>
-				<div>
-					<p
-						aria-label="Author"
-						title="Author"
-						className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
-					>
-						{props.data.author}
-					</p>
-					<p className="text-sm font-medium leading-4 text-gray-600">Author</p>
-				</div>
-			</div>
-			<br></br> */}
-
+		<div className="pt-8 pb-8 pr-2 pl-2 m-2 mt-4 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
 			<p
 				aria-label="Article"
 				title="Jingle Bells"
-				className="flex items-center justify-center inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+				className="flex items-center justify-center mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
 			>
 				{props.data.searchPhrase}
 			</p>
+			<br />
 			<p className="flex mb-3 text-gray-600 text-xs items-center justify-center font-semibold tracking-wide uppercase">
 				<span className=" font-semiboldtext-deep-purple-accent-400">
 					History - {props.data.dateCreated.substring(4, 16)}
@@ -116,31 +67,14 @@ function HistoryCard(props: any) {
 					Filtered By - {filter}
 				</span>
 			</p>
-			<div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-				<Link to="/viewHistory">
-					{/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
-					<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
-						<div className="text-left">
-							<Button
-								text="VIEW HISTORY"
-								size="small"
-								handle={setResultSet}
-								type="view"
-							/>
-						</div>
+			<div className="flex mt-8 space-x-4 md:mt-8 items-center justify-center">
+				<Link to= "/viewHistory">
+					<div className="rounded-sm items-center py-2.5 px-2 text-sm font-semibold text-center text-white bg-dark-cornflower-blue  hover:bg-midnight-blue group hover:shadow">
+						<button	onClick={setResultSet} type="submit"> VIEW HISTORY </button>
 					</div>
 				</Link>
-				{/* <div className="w-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center rounded-full px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
-				<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
-					{/* <button data-testid="btn-report" type="submit" onClick={deleteDraftHandler}>
-						<p className="font-bold">DELETE REPORT</p>
-					</button> */}
-					<Button
-						text="DELETE HISTORY"
-						size="small"
-						handle={deleteHandler}
-						type="delete"
-					/>
+				<div className="rounded-sm inline-flex items-center py-2.5 px-2 text-sm font-semibold text-center bg-crimson  text-white hover:bg-midnight-blue group hover:shadow">
+					<button	onClick={deleteHandler} type="submit"> DELETE HISTORY </button>
 				</div>
 			</div>
 		</div>
