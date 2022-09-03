756731f4a0c4fcb2e5b5bc3dd831a407c96afa0b
Zenthon <u20536951@tuks.co.za>
2022-09-03 21:42:00 +0200

    📱 💄 (UI): Fixed responsiveness of Explore Page

--- app/client/src/components/ExploreCard.tsx
+++ app/client/src/components/ExploreCard.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Button from "./Button";
 
 function ExploreCard(props: any) {
 
@@ -49,13 +48,13 @@ function ExploreCard(props: any) {
 		// 	{/* </div>
 		// </div> */}
 
-		<div className="p-8 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
+		<div className="pt-8 pb-8 pr-2 pl-2 mt-3 mr-2 ml-2 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
 			<div className="flex items-center justify-center">
 				<p aria-label="Author" title="Author" className="mr-3 ">
 					<img
 						src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
 						alt="avatar"
-						className="object-cover w-10 h-10 rounded-full shadow-sm"
+						className="object-cover w-20 h-20 rounded-full shadow-sm"
 					/>
 				</p>
 				<div>
@@ -74,7 +73,7 @@ function ExploreCard(props: any) {
 			<p
 				aria-label="Article"
 				title="Jingle Bells"
-				className="flex items-center justify-center inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+				className="flex items-center justify-center mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
 			>
 				{props.data.title}
 			</p>
@@ -83,23 +82,11 @@ function ExploreCard(props: any) {
 					Published Report - {props.data.dateCreated.substring(4, 16)}
 				</span>
 			</p>
-			<div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-				<Link to={newReportLink}>
-					{/* <div className="w-full rounded-full sm:w-auto bg-twitter-color hover:bg-twitter-color-hover focus:ring-4 focus:outline-none focus:ring-gray-300 text-white inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-400 dark:focus:ring-gray-700"> */}
-					<div className="w-full rounded-full sm:w-auto focus:ring-4 focus:outline-none inline-flex items-center justify-center px-4 py-2.5 ">
-						<div className="text-left">
-							<Button
-								text="View Report"
-								size="small"
-								handle={viewReport}
-								type="view"
-							/>
-							{/* <button data-testid="btn-report" type="submit" onClick={viewReport}>
-									<p className="font-bold">View Report</p>
-								</button> */}
-						</div>
+			<div className="flex mt-8 space-x-4 md:mt-8 items-center justify-center">
+				<Link to = {newReportLink}>
+					<div className="rounded-sm items-center py-2.5 px-16 text-sm font-semibold text-center text-white bg-dark-cornflower-blue  hover:bg-midnight-blue group hover:shadow">
+						<button	onClick={viewReport} type="submit"> VIEW REPORT </button>
 					</div>
-					{/* </div> */}
 				</Link>
 			</div>
 		</div>
