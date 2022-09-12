f475e44573fc8caf01c53b4a5ee3ddd0585cbc23
Tlholo Koma <tlholokoma@gmail.com>
2022-09-12 21:53:54 +0200

    Add implementation to get all table names

--- app/src/functions/backup/handler.ts
+++ app/src/functions/backup/handler.ts
@@ -1,1 +1,36 @@
 import { DynamoDB } from "aws-sdk";
+
+
+export const backupDynamoDB = async () => {
+
+    const dynamoDB = new DynamoDB();
+
+    let params: any = {};
+    let tables: any = [];
+
+    while (true) {
+        const response = await dynamoDB.listTables(params).promise();
+    
+        tables = tables.concat(response.TableNames);
+
+        if (undefined === response.LastEvaluatedTableName) {
+            break;
+        } else {
+            params.ExclusiveStartTableName = response.LastEvaluatedTableName;
+        }
+    }
+
+    tables.map(async () => {
+        // try {
+        //     const date = new Date();
+
+        //     const params = {
+        //         BackupName: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        //         TableName: process.env.TableNames;
+        //     }
+        // }
+    })
+    
+    
+    
+}
