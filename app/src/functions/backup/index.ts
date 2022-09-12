c3dc62547acb44d3b09777d124d566ce6cd37ed0
Tlholo Koma <tlholokoma@gmail.com>
2022-09-13 00:56:15 +0200

    :fire: :wrench: Remove uneccessary properties from lambda function

--- app/src/functions/backup/index.ts
+++ app/src/functions/backup/index.ts
@@ -3,8 +3,6 @@ import { handlerPath } from "@libs/handler-resolver";
 export const backupDynamoDB = {
     handler: `${handlerPath(__dirname)}/handler.backupDynamoDB`,
     description: "A function to create automated backups of the database tables.",
-    memorySize: 128,
-    timeOut: 5,
     events: [
         {
             schedule: "rate(5 minutes)"
