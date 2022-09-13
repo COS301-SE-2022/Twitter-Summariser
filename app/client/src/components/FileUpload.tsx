f582fe6c898e3f7a00394aa75e3584a39909ec14
Zenthon <u20536951@tuks.co.za>
2022-09-14 01:49:17 +0200

    💄 ✨ 🔥 (UI): Introduced file upload component

--- app/client/src/components/FileUpload.tsx
+++ app/client/src/components/FileUpload.tsx
@@ -1,23 +1,32 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function FileUpload() {
+
+    // console.log(files);
+    // setFiles([{
+    //     "name": "File.pdf"
+    // }]);
 
     // const uploadHandler = () => {
     //     console.log("Upload Handler");
     // }
 
-  return (
-    <div className='file-card'>
-        <div className='file-inputs'>
-            <input type='file' />
-            <button type='submit'>
-                <i>
-                    Hello
-                </i>
-            </button>
+    return (
+        <div className='file-card bg-[#edf2f7] border mt-4 p-4 w-full flex items-center justify-between flex-col'>
+            <div className='file-inputs relative mb-6'>
+                <input type='file' className='relative max-w-48 h-12 z-20 opacity-0 cursor-pointer' />
+                <button type='submit' className='absolute mt-6 top-0 left-0 w-10/12 h-full ml-6 z-10 cursor-pointer rounded-md outline-none flex justify-center items-center text-white bg-[#f55e30]'>
+                    <i className='w-6 h-6 bg-white text-[#f55e30] rounded-full flex justify-center items-center mr-2'>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </i>
+                    Upload File
+                </button>
+            </div>
+            <p className='mt-6'>Supported Files:</p>
+            <p>.txt .docx .pdf</p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default FileUpload
