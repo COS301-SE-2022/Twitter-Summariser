f21908e621f46b9a3471568725ed8824fbad58fe
Tlholo Koma <tlholokoma@gmail.com>
2022-09-13 03:19:42 +0200

    Change arn of genScheduledReport lambda funtion

--- app/src/functions/schedulers/handler.ts
+++ app/src/functions/schedulers/handler.ts
@@ -38,7 +38,7 @@ export const reportScheduler = middyfy(
 				Targets: [
 					{
 						Id: ruleName + '-target',
-						Arn: 'arn:aws:lambda:us-east-1:534808114586:function:twitter-summariser-dev-genScheduledReport',
+						Arn: 'arn:aws:lambda:us-east-1:390572845174:function:twitter-summariser-dev-genScheduledReport',
 						Input: JSON.stringify(params.reportDetails),
 					},
 
