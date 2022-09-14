7dc5af580a8f44528224add484c7aee1e52c01ea
Zenthon <u20536951@tuks.co.za>
2022-09-14 16:39:16 +0200

    📱 💄 🔥 (UI): Fixed responsiveness of FileList

--- app/client/src/components/FileList.tsx
+++ app/client/src/components/FileList.tsx
@@ -1,23 +1,16 @@
-import FileItem from "./FileItem"
+import FileItem from "./FileItem";
 
 function FileList(props: any) {
+	const deleteFileHandler = () => {
+		props.removeFile();
+	};
 
-    const deleteFileHandler = () => {        
-        props.removeFile();
-    }
-
-    return (
-        <ul className="ml-1 mt-2">
-            {
-                props.files && props.files.map((f: any) => 
-                    <FileItem
-                        file={f}
-                        deleteFile={deleteFileHandler}
-                    />
-                )
-            }
-    </ul>
-  )
+	return (
+		<ul className="ml-0 mt-2">
+			{props.files &&
+				props.files.map((f: any) => <FileItem file={f} key={f} deleteFile={deleteFileHandler} />)}
+		</ul>
+	);
 }
 
-export default FileList
+export default FileList;
