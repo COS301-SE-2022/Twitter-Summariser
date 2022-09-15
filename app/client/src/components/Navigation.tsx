695f949a484c0304654d5ddc2dfff745cafc0961
GO-Shoderu <go.shoderu@gmail.com>
2022-09-15 17:02:48 +0200

    🐛 💄 (UI): Web Navigation now fully functional with all nav related bugs fixed

--- app/client/src/components/Navigation.tsx
+++ app/client/src/components/Navigation.tsx
@@ -1,14 +1,17 @@
 import { GoHome } from "react-icons/go";
 import { BiHash } from "react-icons/bi";
 import { TiDocumentText } from "react-icons/ti";
+
 import { RiDraftLine } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
-// import { BsShare } from "react-icons/bs";
+
 import { HiOutlineLogin } from "react-icons/hi";
-// import { AiOutlineHistory } from "react-icons/ai";
+
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem } from "@coreui/react";
+import { useState, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
 import useAuth from "../hooks/useAuth";
 import useLogout from "../hooks/useLogout";
 import "./styles/Navigation.css";
@@ -42,16 +45,12 @@ function Navigation() {
 	const [exploreStyle2, changeExploreStyle2] = useState(defaultStyle);
 
 	const [publishStyle, changePublishStyle] = useState(pageDefault);
-	const [publishStyle2, changePublishStyle2] = useState(defaultStyle);
 
 	const [draftsStyle, changeDraftsStyle] = useState(pageDefault);
-	// const [draftsStyle2, changeDraftsStyle2] = useState(defaultStyle);
 
 	const [sharedStyle, changeSharedStyle] = useState(pageDefault);
-	// const [sharedStyle2, changeSharedStyle2] = useState(defaultStyle);
 
 	const [historyStyle, changeHistoryStyle] = useState(pageDefault);
-	// const [historyStyle2, changeHistoryStyle2] = useState(defaultStyle);
 
 	const [summStyle, changeSummStyle] = useState(pageDefault);
 	const [summStyle2, changeSummStyle2] = useState(defaultStyle);
@@ -59,17 +58,10 @@ function Navigation() {
 	const [profileStyle, changeProfileStyle] = useState(pageDefault);
 	const [profileStyle2, changeProfileStyle2] = useState(defaultStyle);
 
-	const [visible, changeVisible] = useState("");
+	const [reportH, setReportH] = useState(false);
 
-	function toggleDrop() {
-		if (visible === "hidden") {
-			changeVisible("hidden");
-			changeVisible("");
-		} else {
-			changeVisible("");
-			changeVisible("hidden");
-		}
-	}
+	const [reportStyle, changeReportStyle] = useState("hidden lg:block");
+	const [reportStyle2, changeReportStyle2] = useState(defaultStyle);
 
 	function homeHandler() {
 		changeHomeStyle(pageCurrent);
@@ -83,15 +75,13 @@ function Navigation() {
 
 		changeHomeStyle2(currentStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(defaultStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 
-		// changeVisible("");
-		// changeVisible("hidden");
+		setReportH(false);
+		changeReportStyle("hidden lg:block");
 	}
 
 	function exploreHandler() {
@@ -106,18 +96,19 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(currentStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(defaultStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 
-		// changeVisible("");
-		// changeVisible("hidden");
+		setReportH(false);
+		changeReportStyle("hidden lg:block");
 	}
 
 	function publishHandler() {
+		setReportH(true);
+		changeReportStyle("hidden lg:block text-blue-800");
+
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageCurrent);
@@ -129,15 +120,15 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(currentStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(currentStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 	}
 
 	function draftHandler() {
+		setReportH(true);
+		changeReportStyle("hidden lg:block text-blue-800");
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageDefault);
@@ -149,15 +140,15 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(currentStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(currentStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 	}
 
 	function sharedHandler() {
+		setReportH(true);
+		changeReportStyle("hidden lg:block text-blue-800");
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageDefault);
@@ -169,15 +160,15 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(currentStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(currentStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 	}
 
 	function historyHandler() {
+		setReportH(true);
+		changeReportStyle("hidden lg:block text-blue-800");
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageDefault);
@@ -189,15 +180,16 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(currentStyle);
+		changeReportStyle2(currentStyle);
+
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(defaultStyle);
 	}
 
 	function summHandler() {
+		setReportH(false);
+		changeReportStyle("hidden lg:block");
+
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageDefault);
@@ -209,18 +201,16 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(defaultStyle);
+
 		changeSummStyle2(currentStyle);
 		changeProfileStyle2(defaultStyle);
-
-		// changeVisible("");
-		// changeVisible("hidden");
 	}
 
 	function profileHandler() {
+		setReportH(false);
+		changeReportStyle("hidden lg:block");
+
 		changeHomeStyle(pageDefault);
 		changeExploreStyle(pageDefault);
 		changePublishStyle(pageDefault);
@@ -232,15 +222,9 @@ function Navigation() {
 
 		changeHomeStyle2(defaultStyle);
 		changeExploreStyle2(defaultStyle);
-		changePublishStyle2(defaultStyle);
-		// changeDraftsStyle2(defaultStyle);
-		// changeSharedStyle2(defaultStyle);
-		// changeHistoryStyle2(defaultStyle);
+		changeReportStyle2(defaultStyle);
 		changeSummStyle2(defaultStyle);
 		changeProfileStyle2(currentStyle);
-
-		// changeVisible("");
-		// changeVisible("hidden");
 	}
 
 	return (
@@ -281,96 +265,190 @@ function Navigation() {
 					</Link>
 				</div>
 
-				<CDropdown>
-					<CDropdownToggle trigger="click">
-						<div
-							className="pt-4 flex flex-row changeColor"
-							key={(3).toString()}
-							onClick={toggleDrop}
-						>
-							<div className="items-center pt-1 ">
-								<TiDocumentText style={publishStyle2} />
+				<Menu as="div" className="relative">
+					<div className="changeColor" key={(3).toString()}>
+						<Menu.Button className=" flex flex-row justify-center bg-white py-2 pt-4 items-center">
+							<div className="items-end">
+								<TiDocumentText style={reportStyle2} />
 							</div>
-							<div className={publishStyle}>&nbsp; Report</div>
-						</div>
-					</CDropdownToggle>
-					<CDropdownMenu>
-						<CDropdownItem className={visible}>
-							<div
-								className="pt-4 flex flex-row changeColor"
-								key={(3).toString()}
-								onClick={publishHandler}
-							>
-								<Link
-									to="/reports"
-									className="flex flex-row items-center"
-									onClick={() => pageHandler("Published")}
-								>
-									{/* <div className="items-center pt-1 ">
-										<TiDocumentText style={publishStyle2} />
-									</div> */}
-									<div className={publishStyle}>
-										&nbsp; &nbsp; &nbsp;Published
-									</div>
-								</Link>
-							</div>
-						</CDropdownItem>
-						<CDropdownItem className={visible}>
-							<div
-								className="pt-4 flex flex-row changeColor"
-								key={(4).toString()}
-								onClick={draftHandler}
-							>
-								<Link
-									to="/drafts"
-									className="flex flex-row items-center"
-									onClick={() => pageHandler("Drafts")}
-								>
-									{/* <div className="items-end pt-1 ">
+							<div className={reportStyle}>&nbsp; Report</div>
+
+							<ChevronDownIcon
+								className={`-mr-1 ml-2 h-5 w-5 ${reportH && " text-blue-800"}`}
+								aria-hidden="true"
+							/>
+						</Menu.Button>
+					</div>
+
+					<Transition
+						as={Fragment}
+						enter="transition ease-out duration-100"
+						enterFrom="transform opacity-0 scale-95"
+						enterTo="transform opacity-100 scale-100"
+						leave="transition ease-in duration-75"
+						leaveFrom="transform opacity-100 scale-100"
+						leaveTo="transform opacity-0 scale-95"
+					>
+						<Menu.Items className="absolute left-2 z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+							<div className="flex flex-col items-center justify-center py-1">
+								<Menu.Item>
+									{() => (
+										<div
+											className="pt-4 flex flex-row changeColor"
+											key={(3).toString()}
+										>
+											<Link
+												to="/reports"
+												className="flex flex-row items-center"
+												onClick={() => pageHandler("Published")}
+											>
+												{/* <div className="items-center pt-1 ">
+													<TiDocumentText style={publishStyle2} />
+												</div> */}
+												<div
+													className={publishStyle}
+													onClick={publishHandler}
+												>
+													Published
+												</div>
+											</Link>
+										</div>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{() => (
+										<div
+											className="pt-4 flex flex-row changeColor"
+											key={(4).toString()}
+										>
+											<Link
+												to="/drafts"
+												className="flex flex-col items-center justify-center"
+												onClick={() => pageHandler("Drafts")}
+											>
+												{/* <div className="items-end pt-1 ">
 										<RiDraftLine style={draftsStyle2} />
 									</div> */}
-									<div className={draftsStyle}>&nbsp; &nbsp; &nbsp;Drafts</div>
-								</Link>
-							</div>
-						</CDropdownItem>
-						<CDropdownItem className={visible}>
-							<div
-								className="pt-4 flex flex-row changeColor"
-								key={(5).toString()}
-								onClick={sharedHandler}
-							>
-								<Link
-									to="/shared"
-									className="flex flex-row items-center"
-									onClick={() => pageHandler("Shared Reports")}
-								>
-									{/* <div className="items-end pt-1 ">
+												<div className={draftsStyle} onClick={draftHandler}>
+													Drafts
+												</div>
+											</Link>
+										</div>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{() => (
+										<div
+											className="pt-4 flex flex-row changeColor"
+											key={(5).toString()}
+										>
+											<Link
+												to="/shared"
+												className="flex flex-row items-center"
+												onClick={() => pageHandler("Shared Reports")}
+											>
+												{/* <div className="items-end pt-1 ">
 										<BsShare style={sharedStyle2} />
 									</div> */}
-									<div className={sharedStyle}>&nbsp; &nbsp; &nbsp;Shared</div>
-								</Link>
-							</div>
-						</CDropdownItem>
-						<CDropdownItem className={visible}>
-							<div
-								className="py-4 flex flex-row changeColor"
-								key={(6).toString()}
-								onClick={historyHandler}
-							>
-								<Link
-									to="/history"
-									className="flex flex-row items-center"
-									onClick={() => pageHandler("Search History")}
-								>
-									{/* <div className="items-end pt-1 ">
+												<div
+													className={sharedStyle}
+													onClick={sharedHandler}
+												>
+													Shared
+												</div>
+											</Link>
+										</div>
+									)}
+								</Menu.Item>
+								<form method="POST" action="#">
+									<Menu.Item>
+										{() => (
+											<div
+												className="py-4 flex flex-row changeColor"
+												key={(6).toString()}
+											>
+												<Link
+													to="/history"
+													className="flex flex-row items-center"
+													onClick={() => pageHandler("Search History")}
+												>
+													{/* <div className="items-end pt-1 ">
 										<AiOutlineHistory style={historyStyle2} />
 									</div> */}
-									<div className={historyStyle}>&nbsp; &nbsp; &nbsp;History</div>
-								</Link>
+													<div
+														className={historyStyle}
+														onClick={historyHandler}
+													>
+														History
+													</div>
+												</Link>
+											</div>
+										)}
+									</Menu.Item>
+								</form>
 							</div>
-						</CDropdownItem>
-					</CDropdownMenu>
-				</CDropdown>
+						</Menu.Items>
+					</Transition>
+				</Menu>
+
+				{/* <button
+					id="dropdownDefault"
+					data-dropdown-toggle="dropdown"
+					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					type="button"
+				>
+					Dropdown button
+				</button> */}
+				{/* <!-- Dropdown menu --> */}
+				{/* <div
+					id="dropdown"
+					className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dropdownStyle"
+					data-popper-reference-hidden=""
+					data-popper-escaped=""
+					data-popper-placement="bottom"
+				>
+					<ul
+						className="py-1 text-sm text-gray-700 dark:text-gray-200"
+						aria-labelledby="dropdownDefault"
+					>
+						<li>
+							<a
+								href=" "
+								className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+								onClick={publishHandler}
+							>
+								Publish
+							</a>
+						</li>
+						<li>
+							<a
+								href=" "
+								className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+								onClick={draftHandler}
+							>
+								Draft
+							</a>
+						</li>
+						<li>
+							<a
+								href=" "
+								className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+								onClick={sharedHandler}
+							>
+								Shared
+							</a>
+						</li>
+						<li>
+							<a
+								href=" "
+								className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+								onClick={historyHandler}
+							>
+								History
+							</a>
+						</li>
+					</ul>
+				</div> */}
 
 				<div
 					className="pt-2 flex flex-row changeColor"
