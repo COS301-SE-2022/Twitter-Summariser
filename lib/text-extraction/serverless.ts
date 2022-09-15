4733ff9bd316f9e00d7761a0d14f1b8dd35352e8
Zenthon <u20536951@tuks.co.za>
2022-09-15 19:38:00 +0200

    ✅ 🚀  (LIB): Finished text extract

---
+++ lib/text-extraction/serverless.ts
@@ -0,0 +1,52 @@
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+    service: "text-extraction",
+    frameworkVersion: "3",
+    provider: {
+        name: "aws",
+		runtime: "nodejs14.x",
+        region: "us-east-1",
+        stage: "dev",
+		apiGateway: {
+			minimumCompressionSize: 1024,
+			shouldStartNameWithService: true
+		},
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+			NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000"
+		}
+    },
+
+    package: {
+		individually: true
+	},
+
+    custom: {
+		esbuild: {
+			bundle: true,
+			minify: false,
+			sourcemap: true,
+			exclude: ["aws-sdk"],
+			target: "node14",
+			define: {
+				"require.resolve": undefined
+			},
+			platform: "node",
+			concurrency: 10
+		}
+	},
+
+    functions: {
+        app: {
+            handler: "handler.handler",
+            events: [
+                {
+                    httpApi: "*"
+                }
+            ]
+        }
+    }
+};
+
+module.exports = serverlessConfiguration;
