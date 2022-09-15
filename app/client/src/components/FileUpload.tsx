8f640cd1f4790ad0e0bbb1a6d18350c7aacd5306
Zenthon <u20536951@tuks.co.za>
2022-09-16 00:35:28 +0200

    💄 📱 🚑 (UI): Fixed file upload component glitch

--- app/client/src/components/FileUpload.tsx
+++ app/client/src/components/FileUpload.tsx
@@ -4,7 +4,7 @@ import links from "../resources/links.json";
 
 const URL = process.env.NODE_ENV === "development" ? links.teLocalhostLink : links.teSeverLink;
 
-function FileUpload(props: any) {
+function FileUpload(this: any, props: any) {
 
 	const uploadHandler = async (event: any) => {
 		props.setFiles([...props.files, event.target.files[0]]);
@@ -50,19 +50,26 @@ function FileUpload(props: any) {
 		event.target.value = null;
 	};
 
+	const uploadFiles = () => {
+		document.getElementById("selectedFile")?.click();
+	};
+
 	return (
 		<div className="file-card bg-[#edf2f7] border mt-4 p-4 w-full flex items-center justify-between flex-col overflow-hidden">
 			<div className="file-inputs relative mb-4">
 				<input
 					type="file"
-					className="relative max-w-48 h-12 z-30 opacity-0 cursor-pointer"
+					id="selectedFile"
+					className="max-w-48 h-14 z-0 top-10  opacity-0 cursor-pointer"
 					accept=".txt, .docx, .pdf"
-					onChange={	uploadHandler	}
-					onClick={	clickHandler 	}
+					style={	{	display: "none"	}	}
+					onChange={	uploadHandler.bind(this) 	}
+					onClick={	clickHandler.bind(this)	 	}
 				/>
 				<button
 					type="submit"
-					className="absolute mt-5 top-0 left-0 w-10/12 h-full ml-6 z-10 cursor-pointer rounded-md outline-none flex justify-center items-center text-white bg-dark-cornflower-blue hover:bg-midnight-blue group hover:shadow transition-colors"
+					onClick={	uploadFiles.bind(this)	}
+					className="items-center cursor-pointer justify-center flex h-12 w-48 md:w-64 text-sm font-semibold text-center text-white bg-dark-cornflower-blue rounded-md  hover:bg-midnight-blue group hover:shadow"
 				>
 					<i className="w-6 h-6 bg-white text-dark-cornflower-blue rounded-full flex justify-center items-center mr-2">
 						<FontAwesomeIcon icon={faPlus} />
