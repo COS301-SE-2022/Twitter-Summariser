af4e07e1b58033ffb59718687af18d579b822369
GO-Shoderu <go.shoderu@gmail.com>
2022-09-18 16:56:32 +0200

    💄 (UI): Removed red button on draft page and added delete confirmation for deleting a draft

--- app/client/src/components/DraftCard.tsx
+++ app/client/src/components/DraftCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
@@ -40,6 +41,12 @@ function DraftCard(props: any) {
 		}
 	};
 
+	const [options, setoptions] = useState(false);
+
+	function optionhandler() {
+		setoptions(!options);
+	}
+
 	return (
 		<div className="pt-5 pb-5 pl-1 pr-1 m-1 mt-2 bg-gradient-to-b from-blue-50 via-sky-100 border rounded-lg transform hover:shadow-md hover:scale-105 transition duration-200 ease-in hover:bg-blue-200">
 			{/* <div className="flex items-center justify-center">
@@ -62,35 +69,68 @@ function DraftCard(props: any) {
 				</div>
 			</div> */}
 			{/* <br /> */}
-
-			<p
-				aria-label="Article"
-				title={props.data.title}
-				className="flex items-center justify-center mb-3 text-xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
-			>
-				{props.data.title}
-			</p>
-			<p className="flex mb-2 text-gray-600 text-xs items-center justify-center font-semibold tracking-wide uppercase">
-				<span className=" font-semiboldtext-deep-purple-accent-400">
-					Draft Report - {props.data.dateCreated.substring(4, 16)}
-				</span>
-			</p>
-			<div className="flex mt-4 space-x-3 items-center justify-center">
-				<Link to={newDraftReportLink}>
-					<div className="items-center py-2.5 px-4 text-xs font-semibold text-center text-white bg-dark-cornflower-blue rounded-md  hover:bg-midnight-blue group hover:shadow">
-						<button onClick={viewDraftReport} type="submit">
-							{" "}
-							VIEW REPORT{" "}
-						</button>
+			{!options ? (
+				<>
+					<p
+						aria-label="Article"
+						title={props.data.title}
+						className="flex items-center justify-center mb-3 text-xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+					>
+						{props.data.title}
+					</p>
+					<p className="flex mb-2 text-gray-600 text-xs items-center justify-center font-semibold tracking-wide uppercase">
+						<span className=" font-semiboldtext-deep-purple-accent-400">
+							Draft Report - {props.data.dateCreated.substring(4, 16)}
+						</span>
+					</p>
+					<div className="flex mt-4 space-x-3 items-center justify-center">
+						<Link to={newDraftReportLink}>
+							<div className="items-center py-2.5 px-4 text-xs font-semibold text-center text-white bg-dark-cornflower-blue rounded-md  hover:bg-midnight-blue group hover:shadow">
+								<button onClick={viewDraftReport} type="submit">
+									{" "}
+									VIEW DRAFT{" "}
+								</button>
+							</div>
+						</Link>
+						<div className="inline-flex items-center py-2.5 px-4 text-xs font-semibold text-center bg-dark-cornflower-blue rounded-md text-white hover:bg-midnight-blue group hover:shadow">
+							<button onClick={optionhandler} type="submit">
+								{" "}
+								DELETE DRAFT{" "}
+							</button>
+						</div>
 					</div>
-				</Link>
-				<div className="inline-flex items-center py-2.5 px-4 text-xs font-semibold text-center bg-crimson rounded-md text-white hover:bg-midnight-blue group hover:shadow">
-					<button onClick={deleteDraftHandler} type="submit">
-						{" "}
-						DELETE REPORT{" "}
-					</button>
-				</div>
-			</div>
+				</>
+			) : (
+				<>
+					{/* <p
+						aria-label="Article"
+						title={props.data.title}
+						className="flex items-center justify-center mb-3 text-xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+					>
+						{props.data.title}
+					</p> */}
+					<p className="flex flex-row mt-2 mb-2 text-gray-600 text-xs text-center items-center justify-center font-semibold tracking-wide uppercase">
+						{/* <span className=" font-semiboldtext-deep-purple-accent-400"> */}
+						Are you sure you want to delete this draft?
+						{/* </span> */}
+					</p>
+					<div className="flex mt-4 mb-2 space-x-3 items-center justify-center">
+						{/* <Link to={newDraftReportLink}> */}
+						<div className="items-center py-2.5 px-4 text-xs font-semibold text-center text-white bg-dark-cornflower-blue rounded-md  hover:bg-midnight-blue group hover:shadow">
+							<button onClick={optionhandler} type="submit">
+								CANCEL
+							</button>
+						</div>
+						{/* </Link> */}
+						<div className="inline-flex items-center py-2.5 px-4 text-xs font-semibold text-center bg-dark-cornflower-blue rounded-md text-white hover:bg-midnight-blue group hover:shadow">
+							<button onClick={deleteDraftHandler} type="submit">
+								{" "}
+								DELETE DRAFT{" "}
+							</button>
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
