4d10a3b5d4eaa966887c1ca1bee32bbc50634372
GO-Shoderu <go.shoderu@gmail.com>
2022-09-19 01:42:13 +0200

    💄(UI): Changed the background color of the cards on the report page

--- app/client/src/components/ReportCard.tsx
+++ app/client/src/components/ReportCard.tsx
@@ -68,7 +68,7 @@ function ReportCard(props: any) {
 		//         </button>
 		//     </div>
 		// </div>
-		<div className="pt-8 pb-8 pr-2 pl-2 mt-3 mr-1 ml-1 bg-white border rounded-lg transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
+		<div className="pt-4 pb-4 pl-1 pr-1 m-2 mt-2 bg-gradient-to-b from-blue-50 via-sky-100 border rounded-lg transform hover:shadow-md hover:scale-105 transition duration-200 ease-in hover:bg-blue-200">
 			<div className="flex items-center justify-center">
 				<p aria-label="Author" title="Author" className="mr-3 ">
 					<img
