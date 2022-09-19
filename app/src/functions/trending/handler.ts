eec3fef1ef838f198edf4f20deb4697c829117ee
malcolmdzimati <malcolmdzimati@gmail.com>
2022-09-19 17:47:17 +0200

    :zap::sparkles: added code to get trending tweets

---
+++ app/src/functions/trending/handler.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { middyfy } from "@libs/lambda";
+import fetch from 'node-fetch'
+import { statusCodes, header } from "@functions/resources/APIresponse";
+
+
+export const searchTweets = middyfy(
+	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+		try {
+			const params = JSON.parse(event.body);
+
+            const response = await fetch('https://api.twitter.com/1.1/trends/place.json?id=1'+params.region, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                return {
+                    statusCode: statusCodes.internalError,
+                    headers: header,
+                    body: JSON.stringify(response.status)
+                };
+            }
+
+			return {
+				statusCode: statusCodes.Successful,
+				headers: header,
+				body: JSON.stringify(response)
+			};
+		} catch (e) {
+			return {
+				statusCode: statusCodes.internalError,
+				headers: header,
+				body: JSON.stringify(e)
+			};
+		}
+	}
+);
