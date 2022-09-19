dae338c7f4079ca1f86028d4e91e71871ad2fcbb
malcolmdzimati <malcolmdzimati@gmail.com>
2022-09-19 18:00:51 +0200

    :bug::zap: created ednpoint

--- app/src/functions/trending/index.ts
+++ app/src/functions/trending/index.ts
@@ -1,6 +1,6 @@
 import { handlerPath } from "@libs/handler-resolver";
 
-export const searchTweets = {
+export const getTrendingTopics = {
 	handler: `${handlerPath(__dirname)}/handler.getTrendingTopics`,
 	description: "Function that return trending topics",
 	events: [
