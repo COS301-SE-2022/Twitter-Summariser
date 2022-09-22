ac17ed371098fc686ce27cdd66202ce8161cb8eb
jonahgasura <u20592061@tuks.co.za>
2022-09-22 23:21:33 +0200

    ✅ ✨ (UI/TEST): Added onChange unit tests for inputs in login

--- app/client/src/__tests__/components/Input.test.tsx
+++ app/client/src/__tests__/components/Input.test.tsx
@@ -1,21 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Input from "../../components/Input";
 
+// mock functions for testing
+const onChange = jest.fn(); // Mock function from jest to provide props
+
+const mockValue = "";
+
+const mockHandler = (event: any) => {
+  event;
+}
+
+const mockFocus = () => {
+  true;
+}
+
+const mockBlur = () => {
+  false;
+}
+
 describe("Rendering Tests For all types of Inputs in the app", () => {
-  // mock functions for testing
-  const mockValue = "";
 
-	const mockHandler = (event: any) => {
-		event;
-	}
-
-	const mockFocus = () => {
-		true;
-	}
-
-	const mockBlur = () => {
-		false;
-	}
 
   // Test to see if email input is rendered for login page
 	it("renders input for login", () => {
@@ -29,6 +33,25 @@ describe("Rendering Tests For all types of Inputs in the app", () => {
 		render(<Input page="login" type="auth" inpFor="password" changeHandler={mockHandler} inputValue={mockValue} place="Password"/>);
 		const input = screen.getByTestId("input_login_password");
 		expect(input).toBeTruthy();
+	});
+});
+
+describe("On change Tests For all types of Inputs in the app", () => {
+
+  // Test to see if email input is changed after receiving input for login page
+	it("tests onChange on email input for login", () => {
+		render(<Input page="login" type="auth" inpFor="email" changeHandler={mockHandler} inputValue={mockValue} focus={mockFocus} blur={mockBlur} place="Email"/>);
+		const input = screen.getByTestId("input_login_email");
+		fireEvent.change(input, { target: { value: "mockEmail" } }); // runs the mock function to change and input email
+    expect(onChange).toHaveBeenCalled;
+	});
+
+  // Test to see if password input is rendered for login page
+	it("tests onChange on password input for login", () => {
+		render(<Input page="login" type="auth" inpFor="password" changeHandler={mockHandler} inputValue={mockValue} place="Password"/>);
+		const input = screen.getByTestId("input_login_password");
+		fireEvent.change(input, { target: { value: "mockPassword" } }); // runs the mock function to change and input email
+    expect(onChange).toHaveBeenCalled;
 	});
 });
 
