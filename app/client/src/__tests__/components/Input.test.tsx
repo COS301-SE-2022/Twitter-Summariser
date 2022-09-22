ba9212c974b55abf01bb7f6d12f3d61d191c9673
jonahgasura <u20592061@tuks.co.za>
2022-09-22 23:35:29 +0200

    ✅ ✨ (UI): Added correct onchange unit tests to Input component

--- app/client/src/__tests__/components/Input.test.tsx
+++ app/client/src/__tests__/components/Input.test.tsx
@@ -24,14 +24,14 @@ describe("Rendering Tests For all types of Inputs in the app", () => {
   // Test to see if email input is rendered for login page
 	it("renders input for login", () => {
 		render(<Input page="login" type="auth" inpFor="email" changeHandler={mockHandler} inputValue={mockValue} focus={mockFocus} blur={mockBlur} place="Email"/>);
-		const input = screen.getByTestId("input_login_email");
+		const input = screen.getByTestId("input_login_email") as HTMLInputElement;
 		expect(input).toBeTruthy();
 	});
 
   // Test to see if password input is rendered for login page
 	it("renders input for login", () => {
 		render(<Input page="login" type="auth" inpFor="password" changeHandler={mockHandler} inputValue={mockValue} place="Password"/>);
-		const input = screen.getByTestId("input_login_password");
+		const input = screen.getByTestId("input_login_password") as HTMLInputElement;
 		expect(input).toBeTruthy();
 	});
 });
@@ -41,7 +41,7 @@ describe("On change Tests For all types of Inputs in the app", () => {
   // Test to see if email input is changed after receiving input for login page
 	it("tests onChange on email input for login", () => {
 		render(<Input page="login" type="auth" inpFor="email" changeHandler={mockHandler} inputValue={mockValue} focus={mockFocus} blur={mockBlur} place="Email"/>);
-		const input = screen.getByTestId("input_login_email");
+		const input = screen.getByTestId("input_login_email") as HTMLInputElement;
 		fireEvent.change(input, { target: { value: "mockEmail" } }); // runs the mock function to change and input email
     expect(onChange).toHaveBeenCalled;
 	});
@@ -49,9 +49,28 @@ describe("On change Tests For all types of Inputs in the app", () => {
   // Test to see if password input is rendered for login page
 	it("tests onChange on password input for login", () => {
 		render(<Input page="login" type="auth" inpFor="password" changeHandler={mockHandler} inputValue={mockValue} place="Password"/>);
-		const input = screen.getByTestId("input_login_password");
+		const input = screen.getByTestId("input_login_password") as HTMLInputElement;
 		fireEvent.change(input, { target: { value: "mockPassword" } }); // runs the mock function to change and input email
     expect(onChange).toHaveBeenCalled;
+	});
+});
+
+describe("Correct change Tests For all types of Inputs in the app", () => {
+
+  // Test to see if email input is changed correctly after receiving input for login page
+	it("tests correct onChange on email input for login", () => {
+		render(<Input page="login" type="auth" inpFor="email" changeHandler={mockHandler} inputValue={mockValue} focus={mockFocus} blur={mockBlur} place="Email"/>);
+		const input = screen.getByTestId("input_login_email") as HTMLInputElement;
+		fireEvent.change(input, { target: { value: "mockEmail" } }); // runs the mock function to change and input email
+    expect(input.value).toBe("mockEmail"); // Checking The condition
+	});
+
+  // Test to see if password input is changed corrctly for login page
+	it("tests correct onChange on password input for login", () => {
+		render(<Input page="login" type="auth" inpFor="password" changeHandler={mockHandler} inputValue={mockValue} place="Password"/>);
+		const input = screen.getByTestId("input_login_password") as HTMLInputElement;
+		fireEvent.change(input, { target: { value: "mockPassword" } }); // runs the mock function to change and input email
+    expect(input.value).toBe("mockPassword"); // Checking The condition
 	});
 });
 
