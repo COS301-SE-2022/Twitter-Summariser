2c2f1463702920a1039529e7d313554095f52675
Zenthon <u20536951@tuks.co.za>
2022-09-22 11:10:37 +0200

    💄 ✨ (UI): Fixed genReport for result set

--- app/client/src/components/ViewHistory.tsx
+++ app/client/src/components/ViewHistory.tsx
@@ -68,20 +68,33 @@ function ViewHistory() {
 		};
 
 		try {
-			const response = await axios.post(
-				"https://betuh6rejrtpyywnwkbckrdnea0bypye.lambda-url.us-east-1.on.aws/",
-				JSON.stringify(searchData),
-				{
-					headers: {
-						"Content-Type": "application/json"
+			if (process.env.NODE_ENV === "production") {
+				const response = await axios.post(
+					"https://betuh6rejrtpyywnwkbckrdnea0bypye.lambda-url.us-east-1.on.aws/",
+					JSON.stringify(searchData),
+					{
+						headers: {
+							"Content-Type": "application/json"
+						}
 					}
-				}
-			);
+				);
 
-			changeGenerateLoading(false);
-			changeDate(await response.data.Report.dateCreated.substring(0, 10));
-			changeDraftReport(response.data.Report.reportID);
-			changeClicked(true);
+				changeGenerateLoading(false);
+				changeDate(await response.data.Report.dateCreated.substring(0, 10));
+				changeDraftReport(response.data.Report.reportID);
+				changeClicked(true);
+			} else {
+				const response = await axiosPrivate.post(
+					"generateReport",
+					JSON.stringify(searchData),
+					{ signal: controller.signal }
+				);
+
+				changeGenerateLoading(false);
+				changeDate(await response.data.Report.dateCreated.substring(0, 10));
+				changeDraftReport(response.data.Report.reportID);
+				changeClicked(true);
+			}
 		} catch (error) {
 			console.error(error);
 		}
