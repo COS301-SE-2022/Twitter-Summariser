c3cc45ef9804e53e31d65e4a098bc2d0166e468d
Isheanesu Joseph Dzingirai <93042354+Zenthon@users.noreply.github.com>
2022-09-25 21:40:26 +0200

    Revert "(API/SERV)🐛 fix for schedual report"

--- app/src/functions/report/handler.ts
+++ app/src/functions/report/handler.ts
@@ -27,24 +27,8 @@ export const generateReport = middyfy(
 
 			const { tweets } = title;
 
-			let id: string;
-			if(params["reportID"] === undefined){
-				id = "RT-" + randomUUID();
-			}else{
-				id = params["reportID"];
-			}
+			let id: string = "RT-" + randomUUID();
 			const d = new Date();
-
-			const report = await ServicesLayer.reportService.addReport({
-				reportID: id,
-				resultSetID: params["resultSetID"],
-				status: "DRAFT",
-				title: title.searchPhrase,
-				apiKey: params["apiKey"],
-				dateCreated: d.toString(),
-				author: params["author"],
-				blockNumber: tweets.length
-			});
 
 			const { data } = await clientV2.get("tweets", { ids: tweets });
 
@@ -91,6 +75,17 @@ export const generateReport = middyfy(
 					position: (x += 2),
 					tweetID: tweet
 				});
+			});
+
+			const report = await ServicesLayer.reportService.addReport({
+				reportID: id,
+				resultSetID: params["resultSetID"],
+				status: "DRAFT",
+				title: title.searchPhrase,
+				apiKey: params["apiKey"],
+				dateCreated: d.toString(),
+				author: params["author"],
+				blockNumber: tweets.length
 			});
 
 			const tb = `BK-${randomUUID()}`;
