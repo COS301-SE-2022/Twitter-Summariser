9393fc2b6f3e83c92807f27446f1f614f7ef47af
Zenthon <u20536951@tuks.co.za>
2022-09-26 10:19:35 +0200

    ✨ ✅ (TEST): Added unit tests for frontend

--- app/client/src/components/TextSummariser.tsx
+++ app/client/src/components/TextSummariser.tsx
@@ -75,7 +75,7 @@ function TextSummariser() {
 	};
 
 	return (
-		<div className=" mt-3 pt-3 ">
+		<div data-testid="summariser" className=" mt-3 pt-3 ">
 			<div className=" mt-4">
 				<div className="flex flex-row justify-around">
 					<h1 className="text-3xl hidden lg:flex lg:flex-row lg:justify-center border-b pb-4 w-5/6 align-middle items-center border-slate-300">
